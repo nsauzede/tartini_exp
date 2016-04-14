c96f5cd465ccb02f05fe56700e7f885fa99765ca
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-04-14 22:10:23 +0200

    Migrate Qt3 code related to multithread to Qt4 code

--- widgets/mainwindow/mainwindow.cpp
+++ widgets/mainwindow/mainwindow.cpp
@@ -721,7 +721,7 @@ void MainWindow::openFile(const char *p_file_name)
 
     gdata->addFileToList(l_new_sound_file);
     gdata->setActiveChannel(&(l_new_sound_file->getChannel(0)));
-    QApplication::postEvent(g_main_window, new QCustomEvent(UPDATE_SLOW));
+    QApplication::postEvent(g_main_window, new QEvent(static_cast<QEvent::Type>(UPDATE_SLOW)));
     gdata->getView().doUpdate();
 }
 

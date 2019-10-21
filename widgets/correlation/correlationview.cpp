ryanbekabe/tartini

Migrate Qt3 code related object name to Qt4 code

widgets/correlation/correlationview.cpp
@@ -40,7 +40,8 @@ CorrelationView::CorrelationView( int viewID_, QWidget *parent )
   correlationWidget = new CorrelationWidget(this);
   QStringList s;
   s << "Chunk correlation" << "Note Aggregate Correlation" << "Note Aggregate Correlation Scaled";
-  QComboBox *aggregateModeComboBox = new QComboBox(this, "aggregateModeComboBox");
+  QComboBox *aggregateModeComboBox = new QComboBox(this);
+  aggregateModeComboBox->setObjectName("aggregateModeComboBox");
   aggregateModeComboBox->addItems(s);
   QHBoxLayout *hLayout = new QHBoxLayout();
   hLayout->setMargin(0);

widgets/freq/freqview.cpp
@@ -130,7 +130,8 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   QHBoxLayout * bottomBottomLayout = new QHBoxLayout();
   bottomLayout->addLayout(bottomBottomLayout);
   
-  QComboBox * amplitudeModeComboBox = new QComboBox(bottomWidget, "amplitudeTypeModeBox");
+  QComboBox * amplitudeModeComboBox = new QComboBox(bottomWidget);
+  amplitudeModeComboBox->setObjectName("amplitudeTypeModeBox");
   amplitudeModeComboBox->setWhatsThis("Select different algorithm parameters to view in the bottom pannel");
   int j;
   QStringList s;
@@ -142,7 +143,8 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   connect(amplitudeModeComboBox, SIGNAL(activated(int)), gdata, SLOT(setAmplitudeMode(int)));
   connect(amplitudeModeComboBox, SIGNAL(activated(int)), amplitudeWidget, SLOT(update()));
 
-  QComboBox * pitchContourModeComboBox = new QComboBox(bottomWidget, "pitchContourModeComboBox");
+  QComboBox * pitchContourModeComboBox = new QComboBox(bottomWidget);
+  pitchContourModeComboBox->setObjectName("pitchContourModeComboBox");
   pitchContourModeComboBox->setWhatsThis("Select whether the Pitch Contour line fades in/out with clarity/loudness of the sound or is a solid dark line");
   s.clear();
   s << "Clarity fading" << "Note grouping";

widgets/htrack/htrackview.cpp
@@ -68,7 +68,8 @@ HTrackView::HTrackView( int viewID_, QWidget *parent ):
   distanceWheel->setTotalAngle(20*360);
   distanceWheel->setToolTip("Move towards/away from piano");
   
-  QPushButton * homeButton = new QPushButton("Reset", this, "homebutton");
+  QPushButton * homeButton = new QPushButton("Reset", this);
+  homeButton->setObjectName("homebutton");
   homeButton->setToolTip("Return to the original view");
   
   QSizeGrip * sizeGrip = new QSizeGrip(this);

widgets/htrack/htrackwidget.cpp
@@ -34,8 +34,9 @@
 
 //------------------------------------------------------------------------------
 HTrackWidget::HTrackWidget(QWidget *parent, const char *name):
-  QGLWidget(parent, name)
+  QGLWidget(parent)
 {
+  setObjectName(name);
 }
 
 //------------------------------------------------------------------------------

widgets/ledindicator.cpp
@@ -24,7 +24,7 @@
 LEDIndicator::LEDIndicator(QPixmap *buffer, QWidget *parent, const char *name, const QColor &on, const QColor &off):
   QWidget(parent)
 {
-  setAccessibleName(name);
+  setObjectName(name);
   setMinimumSize(sizeHint());
   this->on = on;
   this->off = off;
@@ -74,9 +74,9 @@ void LEDIndicator::paintEvent(QPaintEvent *)
 
   QFontMetrics fm = p.fontMetrics();
   int fontHeight = fm.height() / 4;
-  int fontWidth = fm.width(accessibleName()) / 2;
+  int fontWidth = fm.width(objectName()) / 2;
   
-  p.drawText(QWidget::width() / 2 - fontWidth, QWidget::height() / 2 + fontHeight, accessibleName());
+  p.drawText(QWidget::width() / 2 - fontWidth, QWidget::height() / 2 + fontHeight, objectName());
   p.end();
 }
 

widgets/mainwindow/mainwindow.cpp
@@ -182,7 +182,8 @@ MainWindow::MainWindow(void):
 #endif // MYDEBUG
 
   //Create the main Workspace for the view widgets to go in (for MDI)
-  theWorkspace = new QWorkspace(this, "TheWorkspace");
+  theWorkspace = new QWorkspace(this);
+  theWorkspace->setObjectName("TheWorkspace");
   setCentralWidget( theWorkspace );
   
   //Create the file Toolbar

widgets/score/scoreview.cpp
@@ -66,35 +66,45 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
   connect(scaleWheelX, SIGNAL(sliderMoved(double)), scoreWidget, SLOT(setScaleX(double)));
   connect(scaleWheelX, SIGNAL(sliderMoved(double)), scoreWidget, SLOT(update()));
 
-  QComboBox * sharpsModeComboBox = new QComboBox(this, tr("sharpsModeComboBox"));
+  QComboBox * sharpsModeComboBox = new QComboBox(this);
+  sharpsModeComboBox->setObjectName("sharpsModeComboBox");
+
   QStringList s;
   s << tr("Sharps") << tr("Flats");
   sharpsModeComboBox->addItems(s);
   connect(sharpsModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(setSharpsMode(int)));
   connect(sharpsModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(update()));
 
-  QComboBox * notesModeComboBox = new QComboBox(this, tr("notesModeComboBox"));
+  QComboBox * notesModeComboBox = new QComboBox(this);
+  notesModeComboBox->setObjectName("notesModeComboBox");
+
   s.clear();
   s << tr("Notes") << tr("MIDI blocks");
   notesModeComboBox->addItems(s);
   connect(notesModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(setNotesMode(int)));
   connect(notesModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(update()));
 
-  QComboBox * clefModeComboBox = new QComboBox(this, tr("clefModeComboBox"));
+  QComboBox * clefModeComboBox = new QComboBox(this);
+  clefModeComboBox->setObjectName("clefModeComboBox");
+
   s.clear();
   s << tr("Both clefs") << tr("Treble clef");
   clefModeComboBox->addItems(s);
   connect(clefModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(setClefMode(int)));
   connect(clefModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(update()));
 
-  QComboBox * opaqueModeComboBox = new QComboBox(this, tr("opaqueModeComboBox"));
+  QComboBox * opaqueModeComboBox = new QComboBox(this);
+  opaqueModeComboBox->setObjectName("opaqueModeComboBox");
+
   s.clear();
   s << tr("See-through notes") << tr("Opaque notes");
   opaqueModeComboBox->addItems(s);
   connect(opaqueModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(setOpaqueMode(int)));
   connect(opaqueModeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(update()));
 
-  QComboBox * transposeComboBox = new QComboBox(this, tr("transposeComboBox"));
+  QComboBox * transposeComboBox = new QComboBox(this);
+  transposeComboBox->setObjectName("transposeComboBox");
+
   s.clear();
   s << tr("Up 2 octaves") << tr("Up 1 octave") << tr("As played") << tr("Down 1 octave") << tr("Down 2 octaves");
   transposeComboBox->addItems(s);
@@ -102,7 +112,9 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
   connect(transposeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(setTransposeLevel(int)));
   connect(transposeComboBox, SIGNAL(activated(int)), scoreWidget, SLOT(update()));
 
-  QComboBox * showAllModeComboBox = new QComboBox(this, "showAllModeComboBox");
+  QComboBox * showAllModeComboBox = new QComboBox(this);
+  showAllModeComboBox->setObjectName("showAllModeComboBox");
+
   s.clear();
   s << tr("Hide extream notes") << tr("Show all notes");
   showAllModeComboBox->addItems(s);

nsauzede/tartini_exp

widgets/correlation/correlationview.cpp
@@ -42,7 +42,8 @@ CorrelationView::CorrelationView( int p_view_ID
     m_correlation_widget = new CorrelationWidget(this);
     QStringList l_string_list;
     l_string_list << "Chunk correlation" << "Note Aggregate Correlation" << "Note Aggregate Correlation Scaled";
-    QComboBox *l_aggregate_mode_combo_box = new QComboBox(this, "aggregateModeComboBox");
+    QComboBox *l_aggregate_mode_combo_box = new QComboBox(this);
+    l_aggregate_mode_combo_box->setObjectName("aggregateModeComboBox");
     l_aggregate_mode_combo_box->addItems(l_string_list);
     QHBoxLayout *l_h_layout = new QHBoxLayout();
     l_h_layout->setMargin(0);

widgets/freq/freqview.cpp
@@ -145,7 +145,8 @@ FreqView::FreqView( int p_view_id
     QHBoxLayout * l_bottom_bottom_layout = new QHBoxLayout();
     l_bottom_layout->addLayout(l_bottom_bottom_layout);
 
-    QComboBox * l_amplitude_mode_combo_box = new QComboBox(l_bottom_widget, "amplitudeTypeModeBox");
+    QComboBox * l_amplitude_mode_combo_box = new QComboBox(l_bottom_widget);
+    l_amplitude_mode_combo_box->setObjectName("amplitudeTypeModeBox");
     l_amplitude_mode_combo_box->setWhatsThis("Select different algorithm parameters to view in the bottom pannel");
     int l_j;
     QStringList l_string_list;
@@ -157,7 +158,8 @@ FreqView::FreqView( int p_view_id
     connect(l_amplitude_mode_combo_box, SIGNAL(activated(int)), gdata, SLOT(setAmplitudeMode(int)));
     connect(l_amplitude_mode_combo_box, SIGNAL(activated(int)), m_amplitude_widget, SLOT(update()));
 
-    QComboBox * l_pitch_contour_mode_combo_box = new QComboBox(l_bottom_widget, "pitchContourModeComboBox");
+    QComboBox * l_pitch_contour_mode_combo_box = new QComboBox(l_bottom_widget);
+    l_pitch_contour_mode_combo_box->setObjectName("pitchContourModeComboBox");
     l_pitch_contour_mode_combo_box->setWhatsThis("Select whether the Pitch Contour line fades in/out with clarity/loudness of the sound or is a solid dark line");
     l_string_list.clear();
     l_string_list << "Clarity fading" << "Note grouping";
@@ -194,7 +196,7 @@ FreqView::FreqView( int p_view_id
     l_size_policy.setHeightForWidth(false);
     l_size_grip->setSizePolicy(l_size_policy);
     l_bottom_bottom_layout->addWidget(l_size_grip);
-  
+
     //Actually add all the widgets into the widget layout
     l_main_layout->addWidget(l_splitter);
 

widgets/htrack/htrackview.cpp
@@ -91,7 +91,8 @@ HTrackView::HTrackView( int p_view_ID
     m_distance_wheel->setTotalAngle(20 * 360);
     m_distance_wheel->setToolTip("Move towards/away from piano");
 
-    QPushButton * l_home_button = new QPushButton("Reset", this, "homebutton");
+    QPushButton * l_home_button = new QPushButton("Reset", this);
+    l_home_button->setObjectName("homebutton");
     l_home_button->setToolTip("Return to the original view");
 
     QSizeGrip * l_size_grip = new QSizeGrip(this);

widgets/htrack/htrackwidget.cpp
@@ -36,8 +36,9 @@
 HTrackWidget::HTrackWidget( QWidget *p_parent
                           , const char *p_name
                           )
-: QGLWidget(p_parent, p_name)
+: QGLWidget(p_parent)
 {
+  setObjectName(p_name);
 }
 
 //------------------------------------------------------------------------------

widgets/ledindicator.cpp
@@ -29,7 +29,7 @@ LEDIndicator::LEDIndicator(QPixmap *p_buffer
                           )
 : QWidget(p_parent)
 {
-    setAccessibleName(name);
+    setObjectName(p_name);
     setMinimumSize(sizeHint());
     m_on = p_on;
     m_off = p_off;
@@ -79,9 +79,9 @@ void LEDIndicator::paintEvent(QPaintEvent *)
 
     QFontMetrics l_font_metric = l_painter.fontMetrics();
     int l_font_Height = l_font_metric.height() / 4;
-    int l_font_width = l_font_metric.width(accessibleName()) / 2;
+    int l_font_width = l_font_metric.width(objectName()) / 2;
 
-    l_painter.drawText(QWidget::width() / 2 - l_font_width, QWidget::height() / 2 + l_font_Height, accessibleName());
+    l_painter.drawText(QWidget::width() / 2 - l_font_width, QWidget::height() / 2 + l_font_Height, objectName());
     l_painter.end();
 }
 

widgets/mainwindow/mainwindow.cpp
@@ -181,7 +181,8 @@ MainWindow::MainWindow(void)
 #endif // MYDEBUG
 
     //Create the main Workspace for the view widgets to go in (for MDI)
-    m_the_workspace = new QWorkspace(this, "TheWorkspace");
+    m_the_workspace = new QWorkspace(this);
+    m_the_workspace->setObjectName("TheWorkspace");
     setCentralWidget( m_the_workspace );
   
     //Create the file Toolbar
@@ -200,7 +201,7 @@ MainWindow::MainWindow(void)
     l_save_action->setWhatsThis(tr("Save the active sound to a file"));
     l_file_tool_bar->addAction(l_save_action);
     connect(l_save_action, SIGNAL(triggered()), gdata, SLOT(saveActiveFile()));
-  
+
     QAction * l_close_action = new QAction(QIcon(close32x32_xpm), tr("&Close"), this);
     l_close_action->setShortcut(tr("Ctrl+W"));
     l_close_action->setWhatsThis(tr("Close the active sound. If unsaved will ask to save. Note: Asking can be disabled in the preferences"));

widgets/score/scoreview.cpp
@@ -81,35 +81,45 @@ ScoreView::ScoreView(int p_view_id
     connect(l_scale_wheel_X, SIGNAL(wheelMoved(double)), m_score_widget, SLOT(setScaleX(double)));
     connect(l_scale_wheel_X, SIGNAL(wheelMoved(double)), m_score_widget, SLOT(update()));
 
-    QComboBox * l_sharps_mode_combo_box = new QComboBox(this, tr("sharpsModeComboBox"));
+    QComboBox * l_sharps_mode_combo_box = new QComboBox(this);
+    l_sharps_mode_combo_box->setObjectName("sharpsModeComboBox");
+
     QStringList l_string_list;
     l_string_list << tr("Sharps") << tr("Flats");
     l_sharps_mode_combo_box->addItems(l_string_list);
     connect(l_sharps_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(setSharpsMode(int)));
     connect(l_sharps_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(update()));
 
-    QComboBox * l_notes_mode_combo_box = new QComboBox(this, tr("notesModeComboBox"));
+    QComboBox * l_notes_mode_combo_box = new QComboBox(this);
+    l_notes_mode_combo_box->setObjectName("notesModeComboBox");
+
     l_string_list.clear();
     l_string_list << tr("Notes") << tr("MIDI blocks");
     l_notes_mode_combo_box->addItems(l_string_list);
     connect(l_notes_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(setNotesMode(int)));
     connect(l_notes_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(update()));
 
-    QComboBox * l_clef_mode_combo_box = new QComboBox(this, tr("clefModeComboBox"));
+    QComboBox * l_clef_mode_combo_box = new QComboBox(this);
+    l_clef_mode_combo_box->setObjectName("clefModeComboBox");
+
     l_string_list.clear();
     l_string_list << tr("Both clefs") << tr("Treble clef");
     l_clef_mode_combo_box->addItems(l_string_list);
     connect(l_clef_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(setClefMode(int)));
     connect(l_clef_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(update()));
 
-    QComboBox * l_opaque_mode_combo_box = new QComboBox(this, tr("opaqueModeComboBox"));
+    QComboBox * l_opaque_mode_combo_box = new QComboBox(this);
+    l_opaque_mode_combo_box->setObjectName("opaqueModeComboBox");
+
     l_string_list.clear();
     l_string_list << tr("See-through notes") << tr("Opaque notes");
     l_opaque_mode_combo_box->addItems(l_string_list);
     connect(l_opaque_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(setOpaqueMode(int)));
     connect(l_opaque_mode_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(update()));
 
-    QComboBox * l_transpose_combo_box = new QComboBox(this, tr("transposeComboBox"));
+    QComboBox * l_transpose_combo_box = new QComboBox(this);
+    l_transpose_combo_box->setObjectName("transposeComboBox");
+
     l_string_list.clear();
     l_string_list << tr("Up 2 octaves") << tr("Up 1 octave") << tr("As played") << tr("Down 1 octave") << tr("Down 2 octaves");
     l_transpose_combo_box->addItems(l_string_list);
@@ -117,7 +127,9 @@ ScoreView::ScoreView(int p_view_id
     connect(l_transpose_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(setTransposeLevel(int)));
     connect(l_transpose_combo_box, SIGNAL(activated(int)), m_score_widget, SLOT(update()));
 
-    QComboBox * l_show_all_mode_combo_box = new QComboBox(this, "showAllModeComboBox");
+    QComboBox * l_show_all_mode_combo_box = new QComboBox(this);
+    l_show_all_mode_combo_box->setObjectName("showAllModeComboBox");
+
     l_string_list.clear();
     l_string_list << tr("Hide extream notes") << tr("Show all notes");
     l_show_all_mode_combo_box->addItems(l_string_list);
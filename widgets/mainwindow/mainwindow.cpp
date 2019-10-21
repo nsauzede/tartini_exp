ryanbekabe/tartini

Migrate Qt3 code related object name to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -1166,7 +1166,8 @@ TartiniDialog::TartiniDialog(QWidget *parent):
   topLayout->addWidget(tartiniPicture);
   topLayout->addStretch(10);
 
-  QTextBrowser * tartiniTextEdit = new QTextBrowser(this, "TartiniTextEdit");
+  QTextBrowser * tartiniTextEdit = new QTextBrowser(this);
+  tartiniTextEdit->setObjectName("TartiniTextEdit");
   tartiniTextEdit->setOpenExternalLinks(true);
   tartiniTextEdit->setTextFormat(Qt::RichText);
 

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -1208,7 +1208,8 @@ TartiniDialog::TartiniDialog(QWidget *p_parent)
     l_top_layout->addWidget(l_tartini_picture);
     l_top_layout->addStretch(10);
 
-    QTextBrowser * l_tartini_text_edit = new QTextBrowser(this, "TartiniTextEdit");
+    QTextBrowser * l_tartini_text_edit = new QTextBrowser(this);
+    l_tartini_text_edit->setObjectName("TartiniTextEdit");
     l_tartini_text_edit->setOpenExternalLinks(true);
     l_tartini_text_edit->setTextFormat(Qt::RichText);
 
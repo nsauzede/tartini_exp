ryanbekabe/tartini

Update to solve some issues when resizing these views

widgets/hbubble/hbubbleview.cpp
@@ -95,4 +95,10 @@ QSize HBubbleView::sizeHint(void) const
 {
   return QSize(300, 200);
 }
+
+//------------------------------------------------------------------------------
+void HBubbleView::resizeEvent(QResizeEvent *)
+{
+  hBubbleWidget->resize(size());
+}
 // EOF

widgets/hcircle/hcircleview.cpp
@@ -107,4 +107,9 @@ QSize HCircleView::sizeHint(void) const
   return QSize(300, 200);
 }
 
+//------------------------------------------------------------------------------
+void HCircleView::resizeEvent(QResizeEvent *)
+{
+  hCircleWidget->resize(size());
+}
 // EOF

widgets/hstack/hstackview.cpp
@@ -92,4 +92,9 @@ QSize HStackView::sizeHint(void) const
   return QSize(300, 200);
 }
 
+//------------------------------------------------------------------------------
+void HStackView::resizeEvent(QResizeEvent *)
+{
+  hStackWidget->resize(size());
+}
 // EOF

nsauzede/tartini_exp

widgets/hbubble/hbubbleview.cpp
@@ -110,4 +110,10 @@ QSize HBubbleView::sizeHint(void) const
 {
     return QSize(300, 200);
 }
+
+//------------------------------------------------------------------------------
+void HBubbleView::resizeEvent(QResizeEvent *)
+{ 
+    m_h_bubble_widget->resize(size());
+}
 // EOF

widgets/hcircle/hcircleview.cpp
@@ -130,4 +130,9 @@ QSize HCircleView::sizeHint(void) const
     return QSize(300, 200);
 }
 
+//------------------------------------------------------------------------------
+void HCircleView::resizeEvent(QResizeEvent *)
+{
+  m_h_circle_widget->resize(size());
+}
 // EOF

widgets/hstack/hstackview.cpp
@@ -108,4 +108,9 @@ QSize HStackView::sizeHint(void) const
     return QSize(300, 200);
 }
 
+//------------------------------------------------------------------------------
+void HStackView::resizeEvent(QResizeEvent *)
+{
+  m_h_stack_widget->resize(size());
+}
 // EOF
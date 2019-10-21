ryanbekabe/tartini

Migrate Qt3 code related to QTextEdit to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -1169,7 +1169,6 @@ TartiniDialog::TartiniDialog(QWidget *parent):
   QTextBrowser * tartiniTextEdit = new QTextBrowser(this);
   tartiniTextEdit->setObjectName("TartiniTextEdit");
   tartiniTextEdit->setOpenExternalLinks(true);
-  tartiniTextEdit->setTextFormat(Qt::RichText);
 
   QColor linkColor(Qt::cyan);
   QString sheet = QString::fromLatin1("a { text-decoration: underline; color: %1 }").arg(linkColor.name());
@@ -1259,7 +1258,7 @@ GPLDialog::GPLDialog(QWidget *parent):
 
   QTextEdit * GPLTextEdit = new QTextEdit(this, "GPLTextEdit");
   GPLTextEdit->setReadOnly(true);
-  GPLTextEdit->setTextFormat(Qt::PlainText);
+  GPLTextEdit->setAcceptRichText(false);
   QString theText;
   {
     QFile theFile(":res/LICENSE.txt");

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -1211,7 +1211,6 @@ TartiniDialog::TartiniDialog(QWidget *p_parent)
     QTextBrowser * l_tartini_text_edit = new QTextBrowser(this);
     l_tartini_text_edit->setObjectName("TartiniTextEdit");
     l_tartini_text_edit->setOpenExternalLinks(true);
-    l_tartini_text_edit->setTextFormat(Qt::RichText);
 
     QColor l_link_color(Qt::cyan);
     QString l_sheet = QString::fromLatin1("a { text-decoration: underline; color: %1 }").arg(l_link_color.name());
@@ -1301,7 +1300,7 @@ GPLDialog::GPLDialog(QWidget *p_parent)
 
     QTextEdit * l_GPL_text_edit = new QTextEdit(this, "GPLTextEdit");
     l_GPL_text_edit->setReadOnly(true);
-    l_GPL_text_edit->setTextFormat(Qt::PlainText);
+    l_GPL_text_edit->setAcceptRichText(false);
     QString l_the_text;
     {
         QFile l_the_file(":res/LICENSE.txt");
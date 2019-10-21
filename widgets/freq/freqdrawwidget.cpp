ryanbekabe/tartini

Migrate Qt3 code related to SizePolicy to Qt4 code

widgets/freq/freqdrawwidget.cpp
@@ -51,7 +51,8 @@ FreqDrawWidget::FreqDrawWidget(QWidget *parent, const char* name):
   
    dragMode = DragNone;
 
-   QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding, false);
+   QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+   sizePolicy.setHeightForWidth(false);
    setSizePolicy(sizePolicy);
 
    setFocusPolicy(Qt::StrongFocus);

widgets/freq/freqview.cpp
@@ -168,7 +168,9 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   //Create the resize grip. The thing in the bottom right hand corner
   QSizeGrip * sizeGrip = new QSizeGrip(bottomWidget);
   sizeGrip->setFixedSize(15, 15);
-  sizeGrip->setSizePolicy(QSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred, false));
+  QSizePolicy l_size_policy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  l_size_policy.setHeightForWidth(false);
+  sizeGrip->setSizePolicy(l_size_policy);
   bottomBottomLayout->addWidget(sizeGrip);
   
   //Actually add all the widgets into the widget layout

widgets/freq/freqwidgetGL.cpp
@@ -55,7 +55,8 @@ FreqWidgetGL::FreqWidgetGL(QWidget * /*parent*/, const char* /*name*/)
   
    dragMode = DragNone;
 
-   QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding, false);
+   QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+   sizePolicy.setHeightForWidth(false);
    setSizePolicy(sizePolicy);
 
    setFocusPolicy(Qt::StrongFocus);

nsauzede/tartini_exp

widgets/freq/freqdrawwidget.cpp
@@ -53,7 +53,8 @@ FreqDrawWidget::FreqDrawWidget( QWidget *p_parent
 
     m_drag_mode = DragNone;
 
-    QSizePolicy l_size_policy(QSizePolicy::Expanding, QSizePolicy::Expanding, false);
+    QSizePolicy l_size_policy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    l_size_policy.setHeightForWidth(false);
     setSizePolicy(l_size_policy);
 
     setFocusPolicy(Qt::StrongFocus);

widgets/freq/freqview.cpp
@@ -190,7 +190,9 @@ FreqView::FreqView( int p_view_id
     //Create the resize grip. The thing in the bottom right hand corner
     QSizeGrip * l_size_grip = new QSizeGrip(l_bottom_widget);
     l_size_grip->setFixedSize(15, 15);
-    l_size_grip->setSizePolicy(QSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred, false));
+    QSizePolicy l_size_policy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    l_size_policy.setHeightForWidth(false);
+    l_size_grip->setSizePolicy(l_size_policy);
     l_bottom_bottom_layout->addWidget(l_size_grip);
   
     //Actually add all the widgets into the widget layout

widgets/freq/freqwidgetGL.cpp
@@ -57,7 +57,8 @@ FreqWidgetGL::FreqWidgetGL( QWidget * /*parent*/
 
     m_drag_mode = DragNone;
 
-    QSizePolicy l_size_policy(QSizePolicy::Expanding, QSizePolicy::Expanding, false);
+    QSizePolicy l_size_policy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    l_size_policy.setHeightForWidth(false);
     setSizePolicy(l_size_policy);
 
     setFocusPolicy(Qt::StrongFocus);
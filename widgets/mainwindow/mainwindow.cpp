ryanbekabe/tartini

Migrate Qt3 code testing key events shif/alt modifiers to Qt4

widgets/mainwindow/mainwindow.cpp
@@ -562,11 +562,11 @@ void MainWindow::keyPressEvent (QKeyEvent * e)
   View & view = gdata->getView();
   
   double speed = 1;
-  if(e->state() & Qt::ShiftModifier)
+  if(e->QInputEvent::modifiers() & Qt::ShiftModifier)
     {
       speed *= 4;
     }
-  if(e->state() & Qt::ControlModifier)
+  if(e->QInputEvent::modifiers() & Qt::ControlModifier)
     {
       speed *= 10;
     }

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -589,11 +589,11 @@ void MainWindow::keyPressEvent (QKeyEvent * p_event)
     View & l_view = gdata->getView();
 
     double l_speed = 1;
-    if(p_event->state() & Qt::ShiftModifier)
+    if(p_event->QInputEvent::modifiers() & Qt::ShiftModifier)
     {
         l_speed *= 4;
     }
-    if(p_event->state() & Qt::ControlModifier)
+    if(p_event->QInputEvent::modifiers() & Qt::ControlModifier)
     {
         l_speed *= 10;
     }
ryanbekabe/tartini

Migrate Qt3 code related to QString to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -657,7 +657,7 @@ void MainWindow::openFile(void)
     }
   s = QDir::convertSeparators(s);
   gdata->setSettingsValue("Dialogs/openFilesFolder", s);
-  openFile(s.latin1());
+  openFile(s.toStdString().c_str());
 }
 
 //------------------------------------------------------------------------------
@@ -706,7 +706,7 @@ void MainWindow::openRecord(bool andPlay)
   int rate = gdata->getSettingsValue("Sound/sampleRate", 44100);
   QString numberOfChannels = gdata->getSettingsValue("Sound/numberOfChannels", QString("mono"));
   int channels;
-  if(numberOfChannels.lower() == "mono")
+  if(numberOfChannels.toLower() == "mono")
     {
       channels = 1;
     }
@@ -746,11 +746,11 @@ void MainWindow::openRecord(bool andPlay)
 	}
     }
   while(fileExists);
-
-  SoundFile * newSoundFile = new SoundFile();
-  QString newFilename(fileInfo.absFilePath().latin1());
+  
+  SoundFile *newSoundFile = new SoundFile();
+  QString newFilename(fileInfo.absoluteFilePath());
   newFilename = QDir::convertSeparators(newFilename);
-  if(!newSoundFile->openWrite(newFilename, rate, channels, bits, windowSize, stepSize))
+  if(!newSoundFile->openWrite(newFilename.toStdString().c_str(), rate, channels, bits, windowSize, stepSize))
     {
       delete newSoundFile; newSoundFile = NULL;
       return;

widgets/score/scorewidget.cpp
@@ -327,7 +327,7 @@ void ScoreWidget::paintEvent(QPaintEvent *)
   if(ch)
     {
       QString pageString;
-      get_painter().drawText(_fontWidth, height() - 4, pageString.sprintf(tr("Page %d of %d"), si.curPage() + 1, si.numPages()));
+      get_painter().drawText(_fontWidth, height() - 4, pageString.sprintf("Page %d of %d", si.curPage() + 1, si.numPages()));
       while(si.next())
 	{
 	  drawScoreSegment(ch, si.leftX(), si.lineCenterY(), si.leftTime(), si.rightTime());

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -684,7 +684,7 @@ void MainWindow::openFile(void)
     }
     l_file_name = QDir::convertSeparators(l_file_name);
     gdata->setSettingsValue("Dialogs/openFilesFolder", l_file_name);
-    openFile(l_file_name.latin1());
+    openFile(l_file_name.toStdString().c_str());
 }
 
 //------------------------------------------------------------------------------
@@ -733,7 +733,7 @@ void MainWindow::openRecord(bool p_and_play)
     int l_rate = gdata->getSettingsValue("Sound/sampleRate", 44100);
     QString l_number_of_channels = gdata->getSettingsValue("Sound/numberOfChannels", QString("mono"));
     int l_channels;
-    if(l_number_of_channels.lower() == "mono")
+    if(l_number_of_channels.toLower() == "mono")
     {
         l_channels = 1;
     }
@@ -775,9 +775,9 @@ void MainWindow::openRecord(bool p_and_play)
     while(l_file_exists);
 
     SoundFile * l_new_sound_file = new SoundFile();
-    QString l_new_filename(l_file_info.absFilePath().latin1());
+    QString l_new_filename(l_file_info.absoluteFilePath());
     l_new_filename = QDir::convertSeparators(l_new_filename);
-    if(!l_new_sound_file->openWrite(l_new_filename, l_rate, l_channels, l_bits, l_window_size, l_step_size))
+    if(!l_new_sound_file->openWrite(l_new_filename.toStdString().c_str(), l_rate, l_channels, l_bits, l_window_size, l_step_size))
     {
         delete l_new_sound_file; l_new_sound_file = NULL;
         return;

widgets/score/scorewidget.cpp
@@ -352,7 +352,7 @@ void ScoreWidget::paintEvent(QPaintEvent *)
     if(l_active_channel)
     {
         QString l_page_string;
-        get_painter().drawText(m_font_width, height() - 4, l_page_string.sprintf(tr("Page %d of %d"), l_score_segment_iter.curPage() + 1, l_score_segment_iter.numPages()));
+        get_painter().drawText(m_font_width, height() - 4, l_page_string.sprintf("Page %d of %d", l_score_segment_iter.curPage() + 1, l_score_segment_iter.numPages()));
         while(l_score_segment_iter.next())
         {
             drawScoreSegment(l_active_channel, l_score_segment_iter.leftX(), l_score_segment_iter.lineCenterY(), l_score_segment_iter.leftTime(), l_score_segment_iter.rightTime());
ryanbekabe/tartini

Migrate Qt3 code related to multithread to Qt4 code

dialogs/tartinisettingsdialog.cpp
@@ -230,7 +230,7 @@ void TartiniSettingsDialog::saveSettings(void)
 	}
     }
   gdata->syncSettings();
-  QApplication::postEvent(mainWindow, new QCustomEvent(SETTINGS_CHANGED));
+  QApplication::postEvent(mainWindow, new QEvent(static_cast<QEvent::Type>(SETTINGS_CHANGED)));
   TartiniSettingsDialog::accept();
 }
 

nsauzede/tartini_exp

dialogs/tartinisettingsdialog.cpp
@@ -230,7 +230,7 @@ void TartiniSettingsDialog::saveSettings(void)
 	}
     }
   gdata->syncSettings();
-  QApplication::postEvent(g_main_window, new QCustomEvent(SETTINGS_CHANGED));
+  QApplication::postEvent(g_main_window, new QEvent(static_cast<QEvent::Type>(SETTINGS_CHANGED)));
   TartiniSettingsDialog::accept();
 }
 
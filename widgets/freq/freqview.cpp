ryanbekabe/tartini

Migrate Qt3 code related to ToolTip to Qt4

widgets/freq/freqview.cpp
@@ -82,7 +82,7 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   freqWheelY->setWheelWidth(14);
   freqWheelY->setRange(1.6, 5.0, 0.001, 1);
   freqWheelY->setValue(view.logZoomY());
-  QToolTip::add(freqWheelY, "Zoom pitch contour view vertically");
+  freqWheelY->setToolTip("Zoom pitch contour view vertically");
   topRightLayout->addSpacing(20);
   topRightLayout->addWidget(freqWheelY, 0);
   
@@ -117,7 +117,7 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   amplitudeWheelY->setWheelWidth(14);
   amplitudeWheelY->setRange(0.2, 1.00, 0.01, 1);
   amplitudeWheelY->setValue(amplitudeWidget->range());
-  QToolTip::add(amplitudeWheelY, "Zoom pitch contour view vertically");
+  amplitudeWheelY->setToolTip("Zoom pitch contour view vertically");
   bottomTopRightLayout->addWidget(amplitudeWheelY, 0);
   
   //Create the vertical scrollbar
@@ -151,7 +151,7 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   freqWheelX->setWheelWidth(16);
   freqWheelX->setRange(0.5, 9.0, 0.001, 1);
   freqWheelX->setValue(2.0);
-  QToolTip::add(freqWheelX, "Zoom horizontally");
+  freqWheelX->setToolTip("Zoom horizontally");
   
   bottomBottomLayout->addStretch(2);
   bottomBottomLayout->addWidget(amplitudeModeComboBox, 0);

widgets/hbubble/hbubbleview.cpp
@@ -59,7 +59,7 @@ HBubbleView::HBubbleView( int viewID_, QWidget *parent):
   harmonicsWheel->setRange(1, 40, 1, 1);
   harmonicsWheel->setValue(15);
   hBubbleWidget->setNumHarmonics(15);
-  QToolTip::add(harmonicsWheel, "Change number of harmonics shown");
+  harmonicsWheel->setToolTip("Change number of harmonics shown");
   rightLayout->addWidget(harmonicsWheel);
   rightLayout->addStretch(2);
  
@@ -69,7 +69,7 @@ HBubbleView::HBubbleView( int viewID_, QWidget *parent):
   windowSizeWheel->setRange(32, 1024, 2, 1);
   windowSizeWheel->setValue(128);
   hBubbleWidget->setHistoryChunks(128);
-  QToolTip::add(windowSizeWheel, "Change the window size");
+  windowSizeWheel->setToolTip("Change the window size");
   bottomLayout->addWidget(windowSizeWheel);
   bottomLayout->addStretch(2);
   

widgets/hcircle/hcircleview.cpp
@@ -59,7 +59,7 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
   ZoomWheel->setRange(0.001, 0.1, 0.001, 1);
   ZoomWheel->setValue(0.007);
   hCircleWidget->setZoom(0.007);
-  QToolTip::add(ZoomWheel, "Zoom in or out");
+  ZoomWheel->setToolTip("Zoom in or out");
   rightLayout->addWidget(ZoomWheel);
   
   QwtWheel * lowestValueWheel = new QwtWheel(this);
@@ -68,7 +68,7 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
   lowestValueWheel->setRange(-160, 10, 0.01, 1);
   lowestValueWheel->setValue(-100);
   hCircleWidget->setLowestValue(-100);
-  QToolTip::add(lowestValueWheel, "Change the lowest value");
+  lowestValueWheel->setToolTip("Change the lowest value");
   rightLayout->addWidget(lowestValueWheel);
   rightLayout->addStretch(2);
  
@@ -78,7 +78,7 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
   thresholdWheel->setRange(-160, 10, 0.01, 1);
   thresholdWheel->setValue(-100);
   hCircleWidget->setThreshold(-100);
-  QToolTip::add(thresholdWheel, "Change the harmonic threshold");
+  thresholdWheel->setToolTip("Change the harmonic threshold");
   bottomLayout->addWidget(thresholdWheel);
   bottomLayout->addStretch(2);
 

widgets/hstack/hstackview.cpp
@@ -57,7 +57,7 @@ HStackView::HStackView( int viewID_, QWidget * parent):
   dbRangeWheel->setWheelWidth(14);
   dbRangeWheel->setRange(5, 160.0, 0.1, 100);
   dbRangeWheel->setValue(100);
-  QToolTip::add(dbRangeWheel, "Zoom dB range vertically");
+  dbRangeWheel->setToolTip("Zoom dB range vertically");
   rightLayout->addWidget(dbRangeWheel, 0);
   rightLayout->addStretch(2);
 
@@ -66,7 +66,7 @@ HStackView::HStackView( int viewID_, QWidget * parent):
   windowSizeWheel->setWheelWidth(14);
   windowSizeWheel->setRange(32, 1024, 2, 1);
   windowSizeWheel->setValue(128);
-  QToolTip::add(windowSizeWheel, "Zoom windowsize horizontally");
+  windowSizeWheel->setToolTip("Zoom windowsize horizontally");
   bottomLayout->addWidget(windowSizeWheel, 0);
   bottomLayout->addStretch(2); 
 

widgets/htrack/htrackview.cpp
@@ -49,27 +49,27 @@ HTrackView::HTrackView( int viewID_, QWidget *parent ):
     "Tracks alternate in colour for better visibility. It can be seen how the hamonics in a note fit into the musical scale.");
 
   peakThresholdSlider = new QSlider(0, 100, 10, 5, Qt::Vertical, this);
-  QToolTip::add(peakThresholdSlider, "Thresholding of harmonics");
+  peakThresholdSlider->setToolTip("Thresholding of harmonics");
   
   rotateXWheel = new QwtWheel(this);
   rotateXWheel->setWheelWidth(20);
   rotateXWheel->setRange(-180, 180, 0.1, 1);
-  QToolTip::add(rotateXWheel, "Rotate piano horizonally");
+  rotateXWheel->setToolTip("Rotate piano horizonally");
   
   rotateYWheel = new QwtWheel(this);
   rotateYWheel->setOrientation(Qt::Vertical);
   rotateYWheel->setWheelWidth(20);
   rotateYWheel->setRange(-90, 0, 0.1, 1);
-  QToolTip::add(rotateYWheel, "Rotate piano vertically");
+  rotateYWheel->setToolTip("Rotate piano vertically");
   
   distanceWheel = new QwtWheel(this);
   distanceWheel->setOrientation(Qt::Vertical);
   distanceWheel->setRange(100, 5000, 10, 20);
   distanceWheel->setTotalAngle(20*360);
-  QToolTip::add(distanceWheel, "Move towards/away from piano");
+  distanceWheel->setToolTip("Move towards/away from piano");
   
   QPushButton * homeButton = new QPushButton("Reset", this, "homebutton");
-  QToolTip::add(homeButton, "Return to the original view");
+  homeButton->setToolTip("Return to the original view");
   
   QSizeGrip * sizeGrip = new QSizeGrip(this);
   

widgets/mainwindow/mainwindow.cpp
@@ -454,20 +454,20 @@ MainWindow::MainWindow(void):
   noteLabel = new MyLabel("Note: 9999", statusBar(), "notelabel");
   statusBar()->addPermanentWidget(noteLabel, 0);
   setNoteLabel();
-  QToolTip::add(noteLabel, "The current note number in the active file");
+  noteLabel->setToolTip("The current note number in the active file");
   connect(&(gdata->getView()), SIGNAL(onSlowUpdate(double)), this, SLOT(setNoteLabel()));
 
   
   chunkLabel = new MyLabel("Chunk: 999999", statusBar(), "chunklabel");
   statusBar()->addPermanentWidget(chunkLabel, 0);
   setChunkLabel();
-  QToolTip::add(chunkLabel, "The current chunk number in the active file");
+  chunkLabel->setToolTip("The current chunk number in the active file");
   connect(&(gdata->getView()), SIGNAL(onSlowUpdate(double)), this, SLOT(setChunkLabel()));
 
   timeLabel = new MyLabel("Time: -00:00.000", statusBar(), "timelabel");
   statusBar()->addPermanentWidget(timeLabel, 0);
   setTimeLabel(0);
-  QToolTip::add(timeLabel, tr("The current time positon for all files (mins:sec)"));
+  timeLabel->setToolTip(tr("The current time positon for all files (mins:sec)"));
   connect(&(gdata->getView()), SIGNAL(onSlowUpdate(double)), this, SLOT(setTimeLabel(double)));
 
   statusBar()->message( "Ready", 2000 );

widgets/score/scoreview.cpp
@@ -47,7 +47,7 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
   scaleWheelY->setWheelWidth(14);
   scaleWheelY->setRange(1.0, 30.0, 0.1, 1);
   scaleWheelY->setValue(scoreWidget->scaleY());
-  QToolTip::add(scaleWheelY, tr("Zoom vertically"));
+  scaleWheelY->setToolTip(tr("Zoom vertically"));
   rightLayout->addWidget(scaleWheelY, 1);
   rightLayout->addStretch(4);
   connect(scaleWheelY, SIGNAL(sliderMoved(double)), scoreWidget, SLOT(setScaleY(double)));
@@ -59,7 +59,7 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
   scaleWheelX->setWheelWidth(16);
   scaleWheelX->setRange(1.0, 100.0, 1.1, 1);
   scaleWheelX->setValue(scoreWidget->scaleX());
-  QToolTip::add(scaleWheelX, tr("Zoom horizontally"));
+  scaleWheelX->setToolTip(tr("Zoom horizontally"));
   connect(scaleWheelX, SIGNAL(sliderMoved(double)), scoreWidget, SLOT(setScaleX(double)));
   connect(scaleWheelX, SIGNAL(sliderMoved(double)), scoreWidget, SLOT(update()));
 

widgets/tuner/tunerview.cpp
@@ -68,7 +68,7 @@ TunerView::TunerView(int viewID_, QWidget *parent ):
   slider->setRange(0, 2);
   slider->setReadOnly(false);
   layout->addMultiCellWidget(slider, 1, 1, 0, 8);
-  QToolTip::add(slider, tr("Increase slider to smooth the pitch over a longer time period"));
+  slider->setToolTip(tr("Increase slider to smooth the pitch over a longer time period"));
 
   ledBuffer = new QPixmap();
   leds.push_back(new LEDIndicator(ledBuffer, this, "A"));

nsauzede/tartini_exp

widgets/freq/freqview.cpp
@@ -91,7 +91,7 @@ FreqView::FreqView( int p_view_id
     m_freq_wheel_Y->setRange(1.6, 5.0, 0.001, 1);
 #endif // QWT_VERSION >= 0x060000
     m_freq_wheel_Y->setValue(l_view.logZoomY());
-    QToolTip::add(m_freq_wheel_Y, "Zoom pitch contour view vertically");
+    m_freq_wheel_Y->setToolTip("Zoom pitch contour view vertically");
     l_top_right_layout->addSpacing(20);
     l_top_right_layout->addWidget(m_freq_wheel_Y, 0);
   
@@ -132,7 +132,7 @@ FreqView::FreqView( int p_view_id
     m_amplitude_wheel_Y->setRange(0.2, 1.00, 0.01, 1);
 #endif // QWT_VERSION >= 0x060000
     m_amplitude_wheel_Y->setValue(m_amplitude_widget->range());
-    QToolTip::add(m_amplitude_wheel_Y, "Zoom pitch contour view vertically");
+    m_amplitude_wheel_Y->setToolTip("Zoom pitch contour view vertically");
     l_bottom_top_right_layout->addWidget(m_amplitude_wheel_Y, 0);
 
     //Create the vertical scrollbar
@@ -173,7 +173,7 @@ FreqView::FreqView( int p_view_id
     m_freq_wheel_X->setRange(0.5, 9.0, 0.001, 1);
 #endif // QWT_VERSION >= 0x060000
     m_freq_wheel_X->setValue(2.0);
-    QToolTip::add(m_freq_wheel_X, "Zoom horizontally");
+    m_freq_wheel_X->setToolTip("Zoom horizontally");
 
     l_bottom_bottom_layout->addStretch(2);
     l_bottom_bottom_layout->addWidget(l_amplitude_mode_combo_box, 0);

widgets/hbubble/hbubbleview.cpp
@@ -67,7 +67,7 @@ HBubbleView::HBubbleView( int p_view_id
 #endif // QWT_VERSION >= 0x060000
     l_harmonics_wheel->setValue(15);
     m_h_bubble_widget->setNumHarmonics(15);
-    QToolTip::add(l_harmonics_wheel, "Change number of harmonics shown");
+    l_harmonics_wheel->setToolTip("Change number of harmonics shown");
     l_right_layout->addWidget(l_harmonics_wheel);
     l_right_layout->addStretch(2);
 
@@ -84,7 +84,7 @@ HBubbleView::HBubbleView( int p_view_id
 #endif // QWT_VERSION >= 0x060000
     l_window_size_wheel->setValue(128);
     m_h_bubble_widget->setHistoryChunks(128);
-    QToolTip::add(l_window_size_wheel, "Change the window size");
+    l_window_size_wheel->setToolTip("Change the window size");
     l_bottom_layout->addWidget(l_window_size_wheel);
     l_bottom_layout->addStretch(2);
 

widgets/hcircle/hcircleview.cpp
@@ -68,7 +68,7 @@ HCircleView::HCircleView( int p_view_id
 #endif // QWT_VERSION >= 0x060000
     l_zoom_wheel->setValue(0.007);
     m_h_circle_widget->setZoom(0.007);
-    QToolTip::add(l_zoom_wheel, "Zoom in or out");
+    l_zoom_wheel->setToolTip("Zoom in or out");
     l_right_layout->addWidget(l_zoom_wheel);
 
     QwtWheel * l_lowest_value_wheel = new QwtWheel(this);
@@ -84,7 +84,7 @@ HCircleView::HCircleView( int p_view_id
 #endif // QWT_VERSION >= 0x060000
     l_lowest_value_wheel->setValue(-100);
     m_h_circle_widget->setLowestValue(-100);
-    QToolTip::add(l_lowest_value_wheel, "Change the lowest value");
+    l_lowest_value_wheel->setToolTip("Change the lowest value");
     l_right_layout->addWidget(l_lowest_value_wheel);
     l_right_layout->addStretch(2);
  
@@ -101,7 +101,7 @@ HCircleView::HCircleView( int p_view_id
 #endif // QWT_VERSION >= 0x060000
     l_threshold_wheel->setValue(-100);
     m_h_circle_widget->setThreshold(-100);
-    QToolTip::add(l_threshold_wheel, "Change the harmonic threshold");
+    l_threshold_wheel->setToolTip("Change the harmonic threshold");
     l_bottom_layout->addWidget(l_threshold_wheel);
     l_bottom_layout->addStretch(2);
 

widgets/hstack/hstackview.cpp
@@ -66,7 +66,7 @@ HStackView::HStackView( int p_view_id
     l_db_range_wheel->setRange(5, 160.0, 0.1, 100);
 #endif // QWT_VERSION >= 0x060000
     l_db_range_wheel->setValue(100);
-    QToolTip::add(l_db_range_wheel, "Zoom dB range vertically");
+    l_db_range_wheel->setToolTip("Zoom dB range vertically");
     l_right_layout->addWidget(l_db_range_wheel, 0);
     l_right_layout->addStretch(2);
 
@@ -82,7 +82,7 @@ HStackView::HStackView( int p_view_id
     l_window_size_wheel->setRange(32, 1024, 2, 1);
 #endif // QWT_VERSION >= 0x060000
     l_window_size_wheel->setValue(128);
-    QToolTip::add(l_window_size_wheel, "Zoom windowsize horizontally");
+    l_window_size_wheel->setToolTip("Zoom windowsize horizontally");
     l_bottom_layout->addWidget(l_window_size_wheel, 0);
     l_bottom_layout->addStretch(2);
 

widgets/htrack/htrackview.cpp
@@ -51,7 +51,7 @@ HTrackView::HTrackView( int p_view_ID
     "Tracks alternate in colour for better visibility. It can be seen how the hamonics in a note fit into the musical scale.");
 
     m_peak_threshold_slider = new QSlider(0, 100, 10, 5, Qt::Vertical, this);
-    QToolTip::add(m_peak_threshold_slider, "Thresholding of harmonics");
+    m_peak_threshold_slider->setToolTip("Thresholding of harmonics");
 
     m_rotate_X_wheel = new QwtWheel(this);
     m_rotate_X_wheel->setWheelWidth(20);
@@ -63,7 +63,7 @@ HTrackView::HTrackView( int p_view_ID
 #else
     m_rotate_X_wheel->setRange(-180, 180, 0.1, 1);
 #endif // QWT_VERSION >= 0x060000
-    QToolTip::add(m_rotate_X_wheel, "Rotate piano horizonally");
+    m_rotate_X_wheel->setToolTip("Rotate piano horizonally");
 
     m_rotate_Y_wheel = new QwtWheel(this);
     m_rotate_Y_wheel->setOrientation(Qt::Vertical);
@@ -76,7 +76,7 @@ HTrackView::HTrackView( int p_view_ID
 #else
     m_rotate_Y_wheel->setRange(-90, 0, 0.1, 1);
 #endif // QWT_VERSION >= 0x060000
-    QToolTip::add(m_rotate_Y_wheel, "Rotate piano vertically");
+    m_rotate_Y_wheel->setToolTip("Rotate piano vertically");
 
     m_distance_wheel = new QwtWheel(this);
     m_distance_wheel->setOrientation(Qt::Vertical);
@@ -89,13 +89,13 @@ HTrackView::HTrackView( int p_view_ID
     m_distance_wheel->setRange(100, 5000, 10, 20);
 #endif // QWT_VERSION >= 0x060000
     m_distance_wheel->setTotalAngle(20 * 360);
-    QToolTip::add(m_distance_wheel, "Move towards/away from piano");
+    m_distance_wheel->setToolTip("Move towards/away from piano");
 
     QPushButton * l_home_button = new QPushButton("Reset", this, "homebutton");
-    QToolTip::add(l_home_button, "Return to the original view");
+    l_home_button->setToolTip("Return to the original view");
 
     QSizeGrip * l_size_grip = new QSizeGrip(this);
-  
+
     l_main_layout->addWidget(l_frame, 0, 0);
     l_main_layout->addLayout(l_bottom_layout, 1, 0);
     l_main_layout->addLayout(l_right_layout, 0, 1);

widgets/mainwindow/mainwindow.cpp
@@ -481,20 +481,20 @@ MainWindow::MainWindow(void)
     m_note_label = new MyLabel("Note: 9999", statusBar(), "notelabel");
     statusBar()->addPermanentWidget(m_note_label, 0);
     setNoteLabel();
-    QToolTip::add(m_note_label, "The current note number in the active file");
+    m_note_label->setToolTip("The current note number in the active file");
     connect(&(gdata->getView()), SIGNAL(onSlowUpdate(double)), this, SLOT(setNoteLabel()));
 
 
     m_chunk_label = new MyLabel("Chunk: 999999", statusBar(), "chunklabel");
     statusBar()->addPermanentWidget(m_chunk_label, 0);
     setChunkLabel();
-    QToolTip::add(m_chunk_label, "The current chunk number in the active file");
+    m_chunk_label->setToolTip("The current chunk number in the active file");
     connect(&(gdata->getView()), SIGNAL(onSlowUpdate(double)), this, SLOT(setChunkLabel()));
 
     m_time_label = new MyLabel("Time: -00:00.000", statusBar(), "timelabel");
     statusBar()->addPermanentWidget(m_time_label, 0);
     setTimeLabel(0);
-    QToolTip::add(m_time_label, tr("The current time positon for all files (mins:sec)"));
+    m_time_label->setToolTip(tr("The current time positon for all files (mins:sec)"));
     connect(&(gdata->getView()), SIGNAL(onSlowUpdate(double)), this, SLOT(setTimeLabel(double)));
 
     statusBar()->message( "Ready", 2000 );

widgets/score/scoreview.cpp
@@ -56,7 +56,7 @@ ScoreView::ScoreView(int p_view_id
     l_scale_wheel_Y->setRange(1.0, 30.0, 0.1, 1);
 #endif // QWT_VERSION >= 0x060000
     l_scale_wheel_Y->setValue(m_score_widget->scaleY());
-    QToolTip::add(l_scale_wheel_Y, tr("Zoom vertically"));
+    l_scale_wheel_Y->setToolTip(tr("Zoom vertically"));
     l_right_layout->addWidget(l_scale_wheel_Y, 1);
     l_right_layout->addStretch(4);
     connect(l_scale_wheel_Y, SIGNAL(wheelMoved(double)), m_score_widget, SLOT(setScaleY(double)));
@@ -74,7 +74,7 @@ ScoreView::ScoreView(int p_view_id
     l_scale_wheel_X->setRange(1.0, 100.0, 1.1, 1);
 #endif // QWT_VERSION >= 0x060000
     l_scale_wheel_X->setValue(m_score_widget->scaleX());
-    QToolTip::add(l_scale_wheel_X, tr("Zoom horizontally"));
+    l_scale_wheel_X->setToolTip(tr("Zoom horizontally"));
     connect(l_scale_wheel_X, SIGNAL(wheelMoved(double)), m_score_widget, SLOT(setScaleX(double)));
     connect(l_scale_wheel_X, SIGNAL(wheelMoved(double)), m_score_widget, SLOT(update()));
 

widgets/tuner/tunerview.cpp
@@ -80,7 +80,7 @@ TunerView::TunerView(int p_view_iD_
 #endif // QWT_VERSION >= 0x060000
     m_slider->setReadOnly(false);
     l_layout->addMultiCellWidget(m_slider, 1, 1, 0, 8);
-    QToolTip::add(m_slider, tr("Increase slider to smooth the pitch over a longer time period"));
+    m_slider->setToolTip(tr("Increase slider to smooth the pitch over a longer time period"));
 
     m_led_buffer = new QPixmap();
     m_leds.push_back(new LEDIndicator(m_led_buffer, this, "A"));
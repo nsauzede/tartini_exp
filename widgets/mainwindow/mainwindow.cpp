ryanbekabe/tartini

Migrate Qt3 code ( setCaption ) to Qt4 code (setWindowTitle)

widgets/mainwindow/mainwindow.cpp
@@ -1103,22 +1103,22 @@ void MainWindow::setTitle(Channel *ch)
 #ifdef MYDEBUG
   if(ch)
     {
-      setCaption(QString("Tartini (debug) - ") + ch->getUniqueFilename());
+      setWindowTitle(QString("Tartini (debug) - ") + ch->getUniqueFilename());
     }
   else
     {
-      setCaption(QString("Tartini (debug)"));
+      setWindowTitle(QString("Tartini (debug)"));
     }
-#else // MYDEBUG
+#else
   if(ch)
     {
-      setCaption(QString("Tartini - ") + ch->getUniqueFilename());
+      setWindowTitle(QString("Tartini - ") + ch->getUniqueFilename());
     }
   else
     {
-      setCaption(QString("Tartini"));
+      setWindowTitle(QString("Tartini"));
     }
-#endif // MYDEBUG
+#endif
 }
 
 //------------------------------------------------------------------------------

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -1144,20 +1144,20 @@ void MainWindow::setTitle(Channel *p_channel)
 #ifdef MYDEBUG
     if(p_channel)
     {
-        setCaption(QString("Tartini (debug) - ") + p_channel->getUniqueFilename());
+        setWindowTitle(QString("Tartini (debug) - ") + p_channel->getUniqueFilename());
     }
     else
     {
-        setCaption(QString("Tartini (debug)"));
+        setWindowTitle(QString("Tartini (debug)"));
     }
 #else // MYDEBUG
     if(p_channel)
     {
-        setCaption(QString("Tartini - ") + p_channel->getUniqueFilename());
+        setWindowTitle(QString("Tartini - ") + p_channel->getUniqueFilename());
     }
     else
     {
-        setCaption(QString("Tartini"));
+        setWindowTitle(QString("Tartini"));
     }
 #endif // MYDEBUG
 }
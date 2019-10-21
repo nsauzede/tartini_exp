ryanbekabe/tartini

Modify code for qwt_wheel

widgets/freq/freqview.cpp
@@ -80,7 +80,16 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   freqWheelY = new QwtWheel(topWidget);
   freqWheelY->setOrientation(Qt::Vertical);
   freqWheelY->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  freqWheelY->setRange(1.6,5.0);
+  freqWheelY->setSingleStep(0.001);
+  // Use 1000 value for multiplicator so that PageStep has size 1
+  freqWheelY->setPageStepCount(1000);
+#else
   freqWheelY->setRange(1.6, 5.0, 0.001, 1);
+#endif // QWT_VERSION >= 0x060000
+
   freqWheelY->setValue(view.logZoomY());
   QToolTip::add(freqWheelY, "Zoom pitch contour view vertically");
   topRightLayout->addSpacing(20);
@@ -115,7 +124,16 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   amplitudeWheelY = new QwtWheel(bottomWidget);
   amplitudeWheelY->setOrientation(Qt::Vertical);
   amplitudeWheelY->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+  amplitudeWheelY->setRange(0.2, 1.00);
+  amplitudeWheelY->setSingleStep(0.01);
+  // Use 100 value for multiplicator so that PageStep has size 1
+  amplitudeWheelY->setPageStepCount(100);
+#else
   amplitudeWheelY->setRange(0.2, 1.00, 0.01, 1);
+#endif // QWT_VERSION >= 0x060000
+
+
   amplitudeWheelY->setValue(amplitudeWidget->range());
   QToolTip::add(amplitudeWheelY, "Zoom pitch contour view vertically");
   bottomTopRightLayout->addWidget(amplitudeWheelY, 0);
@@ -149,7 +167,14 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   freqWheelX = new QwtWheel(bottomWidget);
   freqWheelX->setOrientation(Qt::Horizontal);
   freqWheelX->setWheelWidth(16);
+#if QWT_VERSION >= 0x060000
+  freqWheelX->setRange(0.5, 9.0);
+  freqWheelX->setSingleStep(0.001);
+  // Use 1000 value for multiplicator so that PageStep has size 1
+  freqWheelX->setPageStepCount(1000);
+#else
   freqWheelX->setRange(0.5, 9.0, 0.001, 1);
+#endif // QWT_VERSION >= 0x060000
   freqWheelX->setValue(2.0);
   QToolTip::add(freqWheelX, "Zoom horizontally");
   

widgets/hbubble/hbubbleview.cpp
@@ -57,7 +57,15 @@ HBubbleView::HBubbleView( int viewID_, QWidget *parent):
   QwtWheel* harmonicsWheel = new QwtWheel(this);
   harmonicsWheel->setOrientation(Qt::Vertical);
   harmonicsWheel->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  harmonicsWheel->setRange(1, 40);
+  harmonicsWheel->setSingleStep(1);
+  harmonicsWheel->setPageStepCount(1);
+#else
   harmonicsWheel->setRange(1, 40, 1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   harmonicsWheel->setValue(15);
   hBubbleWidget->setNumHarmonics(15);
   QToolTip::add(harmonicsWheel, "Change number of harmonics shown");
@@ -67,7 +75,16 @@ HBubbleView::HBubbleView( int viewID_, QWidget *parent):
   QwtWheel* windowSizeWheel = new QwtWheel(this);
   windowSizeWheel->setOrientation(Qt::Horizontal);
   windowSizeWheel->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  windowSizeWheel->setRange(32, 1024);
+  windowSizeWheel->setSingleStep(2);
+  // Sould be 0.5 but put to 1 because 0 zero value disable page stepping
+  windowSizeWheel->setPageStepCount(1);
+#else
   windowSizeWheel->setRange(32, 1024, 2, 1);
+#endif // QWT_VERSION >= 0x060000
+
   windowSizeWheel->setValue(128);
   hBubbleWidget->setHistoryChunks(128);
   QToolTip::add(windowSizeWheel, "Change the window size");

widgets/hcircle/hcircleview.cpp
@@ -57,7 +57,16 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
   QwtWheel * ZoomWheel = new QwtWheel(this);
   ZoomWheel->setOrientation(Qt::Vertical);
   ZoomWheel->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  ZoomWheel->setRange(0.001, 0.1);
+  ZoomWheel->setSingleStep(0.001);
+  // Multiplicator value is 1000 = 1 / 0.001
+  ZoomWheel->setPageStepCount(1000);
+#else
   ZoomWheel->setRange(0.001, 0.1, 0.001, 1);
+#endif // QWT_VERSION >= 0x060000
+
   ZoomWheel->setValue(0.007);
   hCircleWidget->setZoom(0.007);
   QToolTip::add(ZoomWheel, "Zoom in or out");
@@ -66,7 +75,16 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
   QwtWheel * lowestValueWheel = new QwtWheel(this);
   lowestValueWheel->setOrientation(Qt::Vertical);
   lowestValueWheel->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  lowestValueWheel->setRange(-160, 10);
+  lowestValueWheel->setSingleStep(0.01);
+  // Multiplicator value is 100 = 1 / 0.01
+  lowestValueWheel->setPageStepCount(100);
+#else
   lowestValueWheel->setRange(-160, 10, 0.01, 1);
+#endif // QWT_VERSION >= 0x060000
+
   lowestValueWheel->setValue(-100);
   hCircleWidget->setLowestValue(-100);
   QToolTip::add(lowestValueWheel, "Change the lowest value");
@@ -76,7 +94,16 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
   QwtWheel* thresholdWheel = new QwtWheel(this);
   thresholdWheel->setOrientation(Qt::Horizontal);
   thresholdWheel->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  thresholdWheel->setRange(-160, 10);
+  thresholdWheel->setSingleStep(0.01);
+  // Multiplicator value is 100 = 1 / 0.01
+  thresholdWheel->setPageStepCount(100);
+#else
   thresholdWheel->setRange(-160, 10, 0.01, 1);
+#endif // QWT_VERSION >= 0x060000
+
   thresholdWheel->setValue(-100);
   hCircleWidget->setThreshold(-100);
   QToolTip::add(thresholdWheel, "Change the harmonic threshold");

widgets/hstack/hstackview.cpp
@@ -56,7 +56,14 @@ HStackView::HStackView( int viewID_, QWidget * parent):
   QwtWheel * dbRangeWheel = new QwtWheel(this);
   dbRangeWheel->setOrientation(Qt::Vertical);
   dbRangeWheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+  dbRangeWheel->setRange(5, 160.0);
+  dbRangeWheel->setSingleStep(0.1);
+  // Multiplicator value is 1000 = 100 * 0.1
+  dbRangeWheel->setPageStepCount(1000);
+#else
   dbRangeWheel->setRange(5, 160.0, 0.1, 100);
+#endif // QWT_VERSION >= 0x060000
   dbRangeWheel->setValue(100);
   QToolTip::add(dbRangeWheel, "Zoom dB range vertically");
   rightLayout->addWidget(dbRangeWheel, 0);
@@ -65,7 +72,16 @@ HStackView::HStackView( int viewID_, QWidget * parent):
   QwtWheel * windowSizeWheel = new QwtWheel(this);
   windowSizeWheel->setOrientation(Qt::Horizontal);
   windowSizeWheel->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  windowSizeWheel->setRange(32, 1024);
+  windowSizeWheel->setSingleStep(2);
+  // Sould be 0.5 but put to 1 because 0 zero value disable page stepping
+  windowSizeWheel->setPageStepCount(1);
+#else
   windowSizeWheel->setRange(32, 1024, 2, 1);
+#endif // QWT_VERSION >= 0x060000
+
   windowSizeWheel->setValue(128);
   QToolTip::add(windowSizeWheel, "Zoom windowsize horizontally");
   bottomLayout->addWidget(windowSizeWheel, 0);

widgets/htrack/htrackview.cpp
@@ -54,18 +54,45 @@ HTrackView::HTrackView( int viewID_, QWidget *parent ):
   
   rotateXWheel = new QwtWheel(this);
   rotateXWheel->setWheelWidth(20);
+
+#if QWT_VERSION >= 0x060000
+  rotateXWheel->setRange(-180, 180);
+  rotateXWheel->setSingleStep(0.1);
+  // Multiplicator value is 10 = 1 / 0.1
+  rotateXWheel->setPageStepCount(10);
+#else
   rotateXWheel->setRange(-180, 180, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   QToolTip::add(rotateXWheel, "Rotate piano horizonally");
   
   rotateYWheel = new QwtWheel(this);
   rotateYWheel->setOrientation(Qt::Vertical);
   rotateYWheel->setWheelWidth(20);
+
+#if QWT_VERSION >= 0x060000
+  rotateYWheel->setRange(-90, 0);
+  rotateXWheel->setSingleStep(0.1);
+  // Multiplicator value is 10 = 1 / 0.1
+  rotateXWheel->setPageStepCount(10);
+#else
   rotateYWheel->setRange(-90, 0, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   QToolTip::add(rotateYWheel, "Rotate piano vertically");
   
   distanceWheel = new QwtWheel(this);
   distanceWheel->setOrientation(Qt::Vertical);
+
+#if QWT_VERSION >= 0x060000
+  distanceWheel->setRange(100, 5000);
+  distanceWheel->setSingleStep(10);
+  // Multiplicator value is 2 = 20 / 10
+  distanceWheel->setPageStepCount(2);
+#else
   distanceWheel->setRange(100, 5000, 10, 20);
+#endif // QWT_VERSION >= 0x060000
+
   distanceWheel->setTotalAngle(20*360);
   QToolTip::add(distanceWheel, "Move towards/away from piano");
   

widgets/score/scoreview.cpp
@@ -45,7 +45,16 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
   QwtWheel *scaleWheelY = new QwtWheel(this);
   scaleWheelY->setOrientation(Qt::Vertical);
   scaleWheelY->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  scaleWheelY->setRange(1.0, 30.0);
+  scaleWheelY->setSingleStep(0.1);
+  // Multiplicator value is 100 = 1 / 0.01
+  scaleWheelY->setPageStepCount(10);
+#else
   scaleWheelY->setRange(1.0, 30.0, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   scaleWheelY->setValue(scoreWidget->scaleY());
   QToolTip::add(scaleWheelY, tr("Zoom vertically"));
   rightLayout->addWidget(scaleWheelY, 1);
@@ -57,7 +66,15 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
   QwtWheel * scaleWheelX = new QwtWheel(this);
   scaleWheelX->setOrientation(Qt::Horizontal);
   scaleWheelX->setWheelWidth(16);
+
+#if QWT_VERSION >= 0x060000
+  scaleWheelX->setRange(1.0, 100.0);
+  scaleWheelX->setSingleStep(1.1);
+  scaleWheelX->setPageStepCount(1);
+#else
   scaleWheelX->setRange(1.0, 100.0, 1.1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   scaleWheelX->setValue(scoreWidget->scaleX());
   QToolTip::add(scaleWheelX, tr("Zoom horizontally"));
   connect(scaleWheelX, SIGNAL(sliderMoved(double)), scoreWidget, SLOT(setScaleX(double)));

widgets/vibrato/vibratoview.cpp
@@ -241,7 +241,16 @@ VibratoView::VibratoView( int viewID_, QWidget * parent):
   QwtWheel * zoomWheelV = new QwtWheel;
   zoomWheelV->setOrientation(Qt::Vertical);
   zoomWheelV->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  zoomWheelV->setRange(0.3, 25.0);
+  zoomWheelV->setSingleStep(0.1);
+  // Multiplicator value is 10 = 1 / 0.1
+  zoomWheelV->setPageStepCount(10);
+#else
   zoomWheelV->setRange(0.3, 25.0, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   zoomWheelV->setValue(1.0);
   zoomWheelV->setFocusPolicy(Qt::NoFocus);
   QToolTip::add(zoomWheelV, "Zoom vibrato view vertically");
@@ -271,7 +280,15 @@ VibratoView::VibratoView( int viewID_, QWidget * parent):
   QwtWheel *zoomWheelH = new QwtWheel;
   zoomWheelH->setOrientation(Qt::Horizontal);
   zoomWheelH->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+  zoomWheelH->setRange(1, 100);
+  zoomWheelH->setSingleStep(1);
+  zoomWheelH->setPageStepCount(1);
+#else
   zoomWheelH->setRange(1, 100, 1, 1);
+#endif // QWT_VERSION >= 0x060000
+
   zoomWheelH->setValue(25);
   zoomWheelH->setFocusPolicy(Qt::NoFocus);
   QToolTip::add(zoomWheelH, "Zoom vibrato view horizontally");

nsauzede/tartini_exp

widgets/freq/freqview.cpp
@@ -82,7 +82,14 @@ FreqView::FreqView( int p_view_id
     m_freq_wheel_Y = new QwtWheel(l_top_widget);
     m_freq_wheel_Y->setOrientation(Qt::Vertical);
     m_freq_wheel_Y->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    m_freq_wheel_Y->setRange(1.6,5.0);
+    m_freq_wheel_Y->setSingleStep(0.001);
+    // Use 1000 value for multiplicator so that PageStep has size 1
+    m_freq_wheel_Y->setPageStepCount(1000);
+#else
     m_freq_wheel_Y->setRange(1.6, 5.0, 0.001, 1);
+#endif // QWT_VERSION >= 0x060000
     m_freq_wheel_Y->setValue(l_view.logZoomY());
     QToolTip::add(m_freq_wheel_Y, "Zoom pitch contour view vertically");
     l_top_right_layout->addSpacing(20);
@@ -116,7 +123,14 @@ FreqView::FreqView( int p_view_id
     m_amplitude_wheel_Y = new QwtWheel(l_bottom_widget);
     m_amplitude_wheel_Y->setOrientation(Qt::Vertical);
     m_amplitude_wheel_Y->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    m_amplitude_wheel_Y->setRange(0.2, 1.00);
+    m_amplitude_wheel_Y->setSingleStep(0.01);
+    // Use 100 value for multiplicator so that PageStep has size 1
+    m_amplitude_wheel_Y->setPageStepCount(100);
+#else
     m_amplitude_wheel_Y->setRange(0.2, 1.00, 0.01, 1);
+#endif // QWT_VERSION >= 0x060000
     m_amplitude_wheel_Y->setValue(m_amplitude_widget->range());
     QToolTip::add(m_amplitude_wheel_Y, "Zoom pitch contour view vertically");
     l_bottom_top_right_layout->addWidget(m_amplitude_wheel_Y, 0);
@@ -124,7 +138,7 @@ FreqView::FreqView( int p_view_id
     //Create the vertical scrollbar
     m_amplitude_scroll_bar = new MyScrollBar(0.0, 1.0 - m_amplitude_widget->range(), 0.20, m_amplitude_widget->range(), 0, 20, Qt::Vertical, l_bottom_widget);
     l_bottom_top_right_layout->addWidget(m_amplitude_scroll_bar, 4);
-  
+
     Q3BoxLayout * l_bottom_bottom_layout = new Q3HBoxLayout(l_bottom_layout);
 
     QComboBox * l_amplitude_mode_combo_box = new QComboBox(l_bottom_widget, "amplitudeTypeModeBox");
@@ -150,7 +164,14 @@ FreqView::FreqView( int p_view_id
     m_freq_wheel_X = new QwtWheel(l_bottom_widget);
     m_freq_wheel_X->setOrientation(Qt::Horizontal);
     m_freq_wheel_X->setWheelWidth(16);
+#if QWT_VERSION >= 0x060000
+    m_freq_wheel_X->setRange(0.5, 9.0);
+    m_freq_wheel_X->setSingleStep(0.001);
+    // Use 1000 value for multiplicator so that PageStep has size 1
+    m_freq_wheel_X->setPageStepCount(1000);
+#else
     m_freq_wheel_X->setRange(0.5, 9.0, 0.001, 1);
+#endif // QWT_VERSION >= 0x060000
     m_freq_wheel_X->setValue(2.0);
     QToolTip::add(m_freq_wheel_X, "Zoom horizontally");
 

widgets/hbubble/hbubbleview.cpp
@@ -59,7 +59,13 @@ HBubbleView::HBubbleView( int p_view_id
     QwtWheel* l_harmonics_wheel = new QwtWheel(this);
     l_harmonics_wheel->setOrientation(Qt::Vertical);
     l_harmonics_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_harmonics_wheel->setRange(1, 40);
+    l_harmonics_wheel->setSingleStep(1);
+    l_harmonics_wheel->setPageStepCount(1);
+#else
     l_harmonics_wheel->setRange(1, 40, 1, 1);
+#endif // QWT_VERSION >= 0x060000
     l_harmonics_wheel->setValue(15);
     m_h_bubble_widget->setNumHarmonics(15);
     QToolTip::add(l_harmonics_wheel, "Change number of harmonics shown");
@@ -69,7 +75,14 @@ HBubbleView::HBubbleView( int p_view_id
     QwtWheel* l_window_size_wheel = new QwtWheel(this);
     l_window_size_wheel->setOrientation(Qt::Horizontal);
     l_window_size_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_window_size_wheel->setRange(32, 1024);
+    l_window_size_wheel->setSingleStep(2);
+    // Sould be 0.5 but put to 1 because 0 zero value disable page stepping
+    l_window_size_wheel->setPageStepCount(1);
+#else
     l_window_size_wheel->setRange(32, 1024, 2, 1);
+#endif // QWT_VERSION >= 0x060000
     l_window_size_wheel->setValue(128);
     m_h_bubble_widget->setHistoryChunks(128);
     QToolTip::add(l_window_size_wheel, "Change the window size");

widgets/hcircle/hcircleview.cpp
@@ -59,7 +59,14 @@ HCircleView::HCircleView( int p_view_id
     QwtWheel * l_zoom_wheel = new QwtWheel(this);
     l_zoom_wheel->setOrientation(Qt::Vertical);
     l_zoom_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_zoom_wheel->setRange(0.001, 0.1);
+    l_zoom_wheel->setSingleStep(0.001);
+    // Multiplicator value is 1000 = 1 / 0.001
+    l_zoom_wheel->setPageStepCount(1000);
+#else
     l_zoom_wheel->setRange(0.001, 0.1, 0.001, 1);
+#endif // QWT_VERSION >= 0x060000
     l_zoom_wheel->setValue(0.007);
     m_h_circle_widget->setZoom(0.007);
     QToolTip::add(l_zoom_wheel, "Zoom in or out");
@@ -68,7 +75,14 @@ HCircleView::HCircleView( int p_view_id
     QwtWheel * l_lowest_value_wheel = new QwtWheel(this);
     l_lowest_value_wheel->setOrientation(Qt::Vertical);
     l_lowest_value_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_lowest_value_wheel->setRange(-160, 10);
+    l_lowest_value_wheel->setSingleStep(0.01);
+    // Multiplicator value is 100 = 1 / 0.01
+    l_lowest_value_wheel->setPageStepCount(100);
+#else
     l_lowest_value_wheel->setRange(-160, 10, 0.01, 1);
+#endif // QWT_VERSION >= 0x060000
     l_lowest_value_wheel->setValue(-100);
     m_h_circle_widget->setLowestValue(-100);
     QToolTip::add(l_lowest_value_wheel, "Change the lowest value");
@@ -78,7 +92,14 @@ HCircleView::HCircleView( int p_view_id
     QwtWheel* l_threshold_wheel = new QwtWheel(this);
     l_threshold_wheel->setOrientation(Qt::Horizontal);
     l_threshold_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_threshold_wheel->setRange(-160, 10);
+    l_threshold_wheel->setSingleStep(0.01);
+    // Multiplicator value is 100 = 1 / 0.01
+    l_threshold_wheel->setPageStepCount(100);
+#else
     l_threshold_wheel->setRange(-160, 10, 0.01, 1);
+#endif // QWT_VERSION >= 0x060000
     l_threshold_wheel->setValue(-100);
     m_h_circle_widget->setThreshold(-100);
     QToolTip::add(l_threshold_wheel, "Change the harmonic threshold");

widgets/hstack/hstackview.cpp
@@ -58,7 +58,14 @@ HStackView::HStackView( int p_view_id
     QwtWheel * l_db_range_wheel = new QwtWheel(this);
     l_db_range_wheel->setOrientation(Qt::Vertical);
     l_db_range_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_db_range_wheel->setRange(5, 160.0);
+    l_db_range_wheel->setSingleStep(0.1);
+    // Multiplicator value is 1000 = 100 * 0.1
+    l_db_range_wheel->setPageStepCount(1000);
+#else
     l_db_range_wheel->setRange(5, 160.0, 0.1, 100);
+#endif // QWT_VERSION >= 0x060000
     l_db_range_wheel->setValue(100);
     QToolTip::add(l_db_range_wheel, "Zoom dB range vertically");
     l_right_layout->addWidget(l_db_range_wheel, 0);
@@ -67,7 +74,14 @@ HStackView::HStackView( int p_view_id
     QwtWheel * l_window_size_wheel = new QwtWheel(this);
     l_window_size_wheel->setOrientation(Qt::Horizontal);
     l_window_size_wheel->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_window_size_wheel->setRange(32, 1024);
+    l_window_size_wheel->setSingleStep(2);
+    // Sould be 0.5 but put to 1 because 0 zero value disable page stepping
+    l_window_size_wheel->setPageStepCount(1);
+#else
     l_window_size_wheel->setRange(32, 1024, 2, 1);
+#endif // QWT_VERSION >= 0x060000
     l_window_size_wheel->setValue(128);
     QToolTip::add(l_window_size_wheel, "Zoom windowsize horizontally");
     l_bottom_layout->addWidget(l_window_size_wheel, 0);

widgets/htrack/htrackview.cpp
@@ -56,24 +56,45 @@ HTrackView::HTrackView( int p_view_ID
 
     m_rotate_X_wheel = new QwtWheel(this);
     m_rotate_X_wheel->setWheelWidth(20);
+#if QWT_VERSION >= 0x060000
+    m_rotate_X_wheel->setRange(-180, 180);
+    m_rotate_X_wheel->setSingleStep(0.1);
+    // Multiplicator value is 10 = 1 / 0.1
+    m_rotate_X_wheel->setPageStepCount(10);
+#else
     m_rotate_X_wheel->setRange(-180, 180, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
     QToolTip::add(m_rotate_X_wheel, "Rotate piano horizonally");
 
     m_rotate_Y_wheel = new QwtWheel(this);
     m_rotate_Y_wheel->setOrientation(Qt::Vertical);
     m_rotate_Y_wheel->setWheelWidth(20);
+#if QWT_VERSION >= 0x060000
+    m_rotate_Y_wheel->setRange(-90, 0);
+    m_rotate_Y_wheel->setSingleStep(0.1);
+    // Multiplicator value is 10 = 1 / 0.1
+    m_rotate_Y_wheel->setPageStepCount(10);
+#else
     m_rotate_Y_wheel->setRange(-90, 0, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
     QToolTip::add(m_rotate_Y_wheel, "Rotate piano vertically");
 
     m_distance_wheel = new QwtWheel(this);
     m_distance_wheel->setOrientation(Qt::Vertical);
+#if QWT_VERSION >= 0x060000
+    m_distance_wheel->setRange(100, 5000);
+    m_distance_wheel->setSingleStep(10);
+    // Multiplicator value is 2 = 20 / 10
+    m_distance_wheel->setPageStepCount(2);
+#else
     m_distance_wheel->setRange(100, 5000, 10, 20);
+#endif // QWT_VERSION >= 0x060000
     m_distance_wheel->setTotalAngle(20 * 360);
     QToolTip::add(m_distance_wheel, "Move towards/away from piano");
 
     QPushButton * l_home_button = new QPushButton("Reset", this, "homebutton");
     QToolTip::add(l_home_button, "Return to the original view");
-  
+
     QSizeGrip * l_size_grip = new QSizeGrip(this);
   
     l_main_layout->addWidget(l_frame, 0, 0);

widgets/score/scoreview.cpp
@@ -47,7 +47,14 @@ ScoreView::ScoreView(int p_view_id
     QwtWheel *l_scale_wheel_Y = new QwtWheel(this);
     l_scale_wheel_Y->setOrientation(Qt::Vertical);
     l_scale_wheel_Y->setWheelWidth(14);
+#if QWT_VERSION >= 0x060000
+    l_scale_wheel_Y->setRange(1.0, 30.0);
+    l_scale_wheel_Y->setSingleStep(0.1);
+    // Multiplicator value is 100 = 1 / 0.01
+    l_scale_wheel_Y->setPageStepCount(10);
+#else
     l_scale_wheel_Y->setRange(1.0, 30.0, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
     l_scale_wheel_Y->setValue(m_score_widget->scaleY());
     QToolTip::add(l_scale_wheel_Y, tr("Zoom vertically"));
     l_right_layout->addWidget(l_scale_wheel_Y, 1);
@@ -59,7 +66,13 @@ ScoreView::ScoreView(int p_view_id
     QwtWheel * l_scale_wheel_X = new QwtWheel(this);
     l_scale_wheel_X->setOrientation(Qt::Horizontal);
     l_scale_wheel_X->setWheelWidth(16);
+#if QWT_VERSION >= 0x060000
+    l_scale_wheel_X->setRange(1.0, 100.0);
+    l_scale_wheel_X->setSingleStep(1.1);
+    l_scale_wheel_X->setPageStepCount(1);
+#else
     l_scale_wheel_X->setRange(1.0, 100.0, 1.1, 1);
+#endif // QWT_VERSION >= 0x060000
     l_scale_wheel_X->setValue(m_score_widget->scaleX());
     QToolTip::add(l_scale_wheel_X, tr("Zoom horizontally"));
     connect(l_scale_wheel_X, SIGNAL(sliderMoved(double)), m_score_widget, SLOT(setScaleX(double)));

widgets/vibrato/vibratoview.cpp
@@ -244,7 +244,16 @@ VibratoView::VibratoView(int p_view_ID
     QwtWheel * l_zoom_wheel_V = new QwtWheel;
     l_zoom_wheel_V->setOrientation(Qt::Vertical);
     l_zoom_wheel_V->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+    l_zoom_wheel_V->setRange(0.3, 25.0);
+    l_zoom_wheel_V->setSingleStep(0.1);
+    // Multiplicator value is 10 = 1 / 0.1
+    l_zoom_wheel_V->setPageStepCount(10);
+#else
     l_zoom_wheel_V->setRange(0.3, 25.0, 0.1, 1);
+#endif // QWT_VERSION >= 0x060000
+
     l_zoom_wheel_V->setValue(1.0);
     l_zoom_wheel_V->setFocusPolicy(Qt::NoFocus);
     QToolTip::add(l_zoom_wheel_V, "Zoom vibrato view vertically");
@@ -274,7 +283,15 @@ VibratoView::VibratoView(int p_view_ID
     QwtWheel *l_zoom_wheel_H = new QwtWheel;
     l_zoom_wheel_H->setOrientation(Qt::Horizontal);
     l_zoom_wheel_H->setWheelWidth(14);
+
+#if QWT_VERSION >= 0x060000
+    l_zoom_wheel_H->setRange(1, 100);
+    l_zoom_wheel_H->setSingleStep(1);
+    l_zoom_wheel_H->setPageStepCount(1);
+#else
     l_zoom_wheel_H->setRange(1, 100, 1, 1);
+#endif // QWT_VERSION >= 0x060000
+
     l_zoom_wheel_H->setValue(25);
     l_zoom_wheel_H->setFocusPolicy(Qt::NoFocus);
     QToolTip::add(l_zoom_wheel_H, "Zoom vibrato view horizontally");
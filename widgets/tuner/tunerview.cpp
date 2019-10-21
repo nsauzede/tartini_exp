ryanbekabe/tartini

Remove unused parameter

widgets/tuner/tunerview.cpp
@@ -72,15 +72,15 @@ TunerView::TunerView(int viewID_, QWidget *parent ):
   slider->setToolTip(tr("Increase slider to smooth the pitch over a longer time period"));
 
   ledBuffer = new QPixmap();
-  leds.push_back(new LEDIndicator(ledBuffer, this, "A"));
-  leds.push_back(new LEDIndicator(ledBuffer, this, "B"));
-  leds.push_back(new LEDIndicator(ledBuffer, this, "C"));
-  leds.push_back(new LEDIndicator(ledBuffer, this, "D"));
-  leds.push_back(new LEDIndicator(ledBuffer, this, "E"));
-  leds.push_back(new LEDIndicator(ledBuffer, this, "F"));
-  leds.push_back(new LEDIndicator(ledBuffer, this, "G"));
+  leds.push_back(new LEDIndicator(this, "A"));
+  leds.push_back(new LEDIndicator(this, "B"));
+  leds.push_back(new LEDIndicator(this, "C"));
+  leds.push_back(new LEDIndicator(this, "D"));
+  leds.push_back(new LEDIndicator(this, "E"));
+  leds.push_back(new LEDIndicator(this, "F"));
+  leds.push_back(new LEDIndicator(this, "G"));
 
-  leds.push_back(new LEDIndicator(ledBuffer, this, "#"));
+  leds.push_back(new LEDIndicator(this, "#"));
 
 
   // Add the leds for note names into the positions (1, 0) to (1, 6)

nsauzede/tartini_exp

widgets/tuner/tunerview.cpp
@@ -84,15 +84,15 @@ TunerView::TunerView(int p_view_iD_
     m_slider->setToolTip(tr("Increase slider to smooth the pitch over a longer time period"));
 
     m_led_buffer = new QPixmap();
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "A"));
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "B"));
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "C"));
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "D"));
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "E"));
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "F"));
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "G"));
+    m_leds.push_back(new LEDIndicator(this, "A"));
+    m_leds.push_back(new LEDIndicator(this, "B"));
+    m_leds.push_back(new LEDIndicator(this, "C"));
+    m_leds.push_back(new LEDIndicator(this, "D"));
+    m_leds.push_back(new LEDIndicator(this, "E"));
+    m_leds.push_back(new LEDIndicator(this, "F"));
+    m_leds.push_back(new LEDIndicator(this, "G"));
 
-    m_leds.push_back(new LEDIndicator(m_led_buffer, this, "#"));
+    m_leds.push_back(new LEDIndicator(this, "#"));
 
     // Add the leds for note names into the positions (1, 0) to (1, 6)
     for(int l_n = 0; l_n < 7; l_n++)
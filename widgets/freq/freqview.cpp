ryanbekabe/tartini

Migrate Qt3 code related to GLWIdget to Qt4 code

widgets/freq/freqview.cpp
@@ -220,14 +220,14 @@ void FreqView::zoomIn(void)
   bool doneIt = false;
   if(gdata->getRunning() != STREAM_FORWARD)
     {
-      if(freqWidgetGL->hasMouse())
+      if(freqWidgetGL->testAttribute(Qt::WA_UnderMouse))
 	{
 	  QPoint mousePos = freqWidgetGL->mapFromGlobal(QCursor::pos());
 	  gdata->getView().setZoomFactorX(gdata->getView().logZoomX() + 0.1, mousePos.x());
 	  gdata->getView().setZoomFactorY(gdata->getView().logZoomY() + 0.1, freqWidgetGL->height() - mousePos.y());
 	  doneIt = true;
 	}
-      else if(amplitudeWidget->hasMouse())
+      else if(amplitudeWidget->testAttribute(Qt::WA_UnderMouse))
 	{
 	  QPoint mousePos = amplitudeWidget->mapFromGlobal(QCursor::pos());
 	  gdata->getView().setZoomFactorX(gdata->getView().logZoomX() + 0.1, mousePos.x());
@@ -246,7 +246,7 @@ void FreqView::zoomIn(void)
 void FreqView::zoomOut(void)
 {
   gdata->getView().setZoomFactorX(gdata->getView().logZoomX() - 0.1);
-  if(!amplitudeWidget->hasMouse())
+  if(!amplitudeWidget->testAttribute(Qt::WA_UnderMouse))
     {
       gdata->getView().setZoomFactorY(gdata->getView().logZoomY() - 0.1);
     }

nsauzede/tartini_exp

widgets/freq/freqview.cpp
@@ -242,14 +242,14 @@ void FreqView::zoomIn(void)
     bool l_done_it = false;
     if(gdata->getRunning() != STREAM_FORWARD)
     {
-        if(m_freq_widget_GL->hasMouse())
+        if(m_freq_widget_GL->testAttribute(Qt::WA_UnderMouse))
         {
             QPoint l_mouse_pos = m_freq_widget_GL->mapFromGlobal(QCursor::pos());
             gdata->getView().setZoomFactorX(gdata->getView().logZoomX() + 0.1, l_mouse_pos.x());
             gdata->getView().setZoomFactorY(gdata->getView().logZoomY() + 0.1, m_freq_widget_GL->height() - l_mouse_pos.y());
             l_done_it = true;
         }
-        else if(m_amplitude_widget->hasMouse())
+        else if(m_amplitude_widget->testAttribute(Qt::WA_UnderMouse))
         {
             QPoint l_mouse_pos = m_amplitude_widget->mapFromGlobal(QCursor::pos());
             gdata->getView().setZoomFactorX(gdata->getView().logZoomX() + 0.1, l_mouse_pos.x());
@@ -269,7 +269,7 @@ void FreqView::zoomIn(void)
 void FreqView::zoomOut(void)
 {
     gdata->getView().setZoomFactorX(gdata->getView().logZoomX() - 0.1);
-    if(!m_amplitude_widget->hasMouse())
+    if(!m_amplitude_widget->testAttribute(Qt::WA_UnderMouse))
     {
         gdata->getView().setZoomFactorY(gdata->getView().logZoomY() - 0.1);
     }
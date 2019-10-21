ryanbekabe/tartini

Migration from Qt2 to Qt4 : remove deprecated name parameter from QWidget constructor

widgets/ledindicator.cpp
@@ -22,8 +22,9 @@
 
 //------------------------------------------------------------------------------
 LEDIndicator::LEDIndicator(QPixmap *buffer, QWidget *parent, const char *name, const QColor &on, const QColor &off):
-  QWidget(parent, name)
+  QWidget(parent)
 {
+  setAccessibleName(name);
   setMinimumSize(sizeHint());
   this->on = on;
   this->off = off;
@@ -82,9 +83,9 @@ void LEDIndicator::paintEvent(QPaintEvent *)
 
   QFontMetrics fm = p.fontMetrics();
   int fontHeight = fm.height() / 4;
-  int fontWidth = fm.width(name()) / 2;
+  int fontWidth = fm.width(accessibleName()) / 2;
   
-  p.drawText(QWidget::width() / 2 - fontWidth, QWidget::height() / 2 + fontHeight, name());
+  p.drawText(QWidget::width() / 2 - fontWidth, QWidget::height() / 2 + fontHeight, accessibleName());
   p.end();
 
   // Swap buffers

nsauzede/tartini_exp

widgets/ledindicator.cpp
@@ -27,8 +27,9 @@ LEDIndicator::LEDIndicator(QPixmap *p_buffer
                           ,const QColor & p_on
                           ,const QColor &p_off
                           )
-: QWidget(p_parent, p_name)
+: QWidget(p_parent)
 {
+    setAccessibleName(name);
     setMinimumSize(sizeHint());
     m_on = p_on;
     m_off = p_off;
@@ -87,9 +88,9 @@ void LEDIndicator::paintEvent(QPaintEvent *)
 
     QFontMetrics l_font_metric = l_painter.fontMetrics();
     int l_font_Height = l_font_metric.height() / 4;
-    int l_font_width = l_font_metric.width(name()) / 2;
+    int l_font_width = l_font_metric.width(accessibleName()) / 2;
 
-    l_painter.drawText(QWidget::width() / 2 - l_font_width, QWidget::height() / 2 + l_font_Height, name());
+    l_painter.drawText(QWidget::width() / 2 - l_font_width, QWidget::height() / 2 + l_font_Height, accessibleName());
     l_painter.end();
 
     // Swap buffers
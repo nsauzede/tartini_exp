ryanbekabe/tartini

Migrate Qt3 code related to QSPinBox to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -457,7 +457,9 @@ MainWindow::MainWindow(void):
     "Note: For other scales the root note is chosen from the even-tempered scale with that 'A'."));
   addToolBar(freqAToolBar);
 
-  QSpinBox * freqASpinBox = new QSpinBox(400, 600, 1, freqAToolBar, "freqASpinBox");
+  QSpinBox * freqASpinBox = new QSpinBox(freqAToolBar);
+  freqASpinBox->setRange(400,600);
+  freqASpinBox->setSingleStep(1);
   freqASpinBox->setPrefix("A=");
   freqASpinBox->setSuffix(" Hz");
   freqASpinBox->setFocusPolicy(Qt::NoFocus);

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -484,7 +484,9 @@ MainWindow::MainWindow(void)
                                    );
     addToolBar(l_freq_A_tool_bar);
 
-    QSpinBox * l_freq_A_spin_box = new QSpinBox(400, 600, 1, l_freq_A_tool_bar, "freqASpinBox");
+    QSpinBox * l_freq_A_spin_box = new QSpinBox(l_freq_A_tool_bar);
+    l_freq_A_spin_box->setRange(400,600);
+    l_freq_A_spin_box->setSingleStep(1);
     l_freq_A_spin_box->setPrefix("A=");
     l_freq_A_spin_box->setSuffix(" Hz");
     l_freq_A_spin_box->setFocusPolicy(Qt::NoFocus);
ryanbekabe/tartini

Migrate Qt3 code related to QVBoxLayout to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -1155,9 +1155,11 @@ TartiniDialog::TartiniDialog(QWidget *parent):
 {
   setCaption("About Tartini - Version " TARTINI_VERSION_STR);
   setBackgroundColor(Qt::black);
-  QVBoxLayout * layout = new QVBoxLayout(this, 6, 6);
 
   QHBoxLayout * topLayout = new QHBoxLayout(layout);
+  QVBoxLayout * layout = new QVBoxLayout(this);
+  layout->setContentsMargins(6,6,6,6);
+  layout->setSpacing(6);
   QLabel * tartiniPicture = new QLabel(this);
   tartiniPicture->setObjectName("TartiniPicture");
   QPixmap tartiniPixmap(":/res/tartinilogo.jpg", "JPG");
@@ -1255,8 +1257,10 @@ GPLDialog::GPLDialog(QWidget *parent):
   QDialog(parent, NULL, true)
 {
   setCaption("GPL Licence");
-  QVBoxLayout * layout = new QVBoxLayout(this, 6, 6);
 
+  QVBoxLayout * layout = new QVBoxLayout(this);
+  layout->setContentsMargins(6,6,6,6);
+  layout->setSpacing(6);
   QTextEdit * GPLTextEdit = new QTextEdit(this);
   GPLTextEdit->setObjectName("GPLTextEdit");
   GPLTextEdit->setReadOnly(true);

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -1197,8 +1197,10 @@ TartiniDialog::TartiniDialog(QWidget *p_parent)
 {
     setCaption("About Tartini - Version " TARTINI_VERSION_STR);
     setBackgroundColor(Qt::black);
-    QVBoxLayout * l_layout = new QVBoxLayout(this, 6, 6);
 
+    QVBoxLayout * l_layout = new QVBoxLayout(this);
+    l_layout->setContentsMargins(6,6,6,6);
+    l_layout->setSpacing(6);
     QHBoxLayout * l_top_layout = new QHBoxLayout(l_layout);
     QLabel * l_tartini_picture = new QLabel(this);
     l_tartini_picture->setObjectName("TartiniPicture");
@@ -1297,8 +1299,10 @@ GPLDialog::GPLDialog(QWidget *p_parent)
 : QDialog(p_parent, NULL, true)
 {
     setCaption("GPL Licence");
-    QVBoxLayout * l_layout = new QVBoxLayout(this, 6, 6);
 
+    QVBoxLayout * l_layout = new QVBoxLayout(this);
+    l_layout->setContentsMargins(6,6,6,6);
+    l_layout->setSpacing(6);
     QTextEdit * l_GPL_text_edit = new QTextEdit(this);
     l_GPL_text_edit->setObjectName("GPLTextEdit");
     l_GPL_text_edit->setReadOnly(true);
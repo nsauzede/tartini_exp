ryanbekabe/tartini

Migrate Qt3 code using colorGroup to Qt4 code using QPalette

widgets/freq/freqdrawwidget.cpp
@@ -25,6 +25,7 @@
 #include <QKeyEvent>
 #include <QEvent>
 #include <QPaintEvent>
+#include <QPalette>
 
 // zoom cursors
 #include "pics/zoomx.xpm"
@@ -182,10 +183,12 @@ void FreqDrawWidget::paintEvent(QPaintEvent *)
       drawChannel(*this, ch, get_painter(), view.viewLeft(), view.currentTime(), view.zoomX(), view.viewBottom(), view.zoomY(), DRAW_VIEW_NORMAL);
     }
 
+  QPalette l_palette;
+  QColor l_foreground = l_palette.color(QPalette::WindowText);
   // Draw a light grey band indicating which time is being used in the current window
   if(gdata->getActiveChannel())
     {
-      QColor lineColor = colorGroup().foreground();
+      QColor lineColor = l_foreground;
       lineColor.setAlpha(50);
       Channel *ch = gdata->getActiveChannel();
       double halfWindowTime = (double)ch->size() / (double)(ch->rate() * 2);
@@ -195,7 +198,7 @@ void FreqDrawWidget::paintEvent(QPaintEvent *)
     }
 
   // Draw the current time line
-  get_painter().setPen(QPen(colorGroup().foreground(), 1));
+  get_painter().setPen(QPen(l_foreground, 1));
   get_painter().drawLine(curTimePixel, 0, curTimePixel, height() - 1);
 
   endDrawing();

widgets/hblock/hblockwidget.cpp
@@ -58,8 +58,10 @@ void HBlockWidget::paintEvent( QPaintEvent * )
   
 	  // Work out the bar height for each harmonic
 	  double barHeight = double(height()) / double(data.getHarmonicFreqSize());
-	  QColor fillColor = colorBetween(colorGroup().background(), active->get_color(), data.getVolumeValue(*gdata));
-	  QColor outlineColor = colorBetween(colorGroup().background(), Qt::black, data.getVolumeValue(*gdata));
+	  QPalette l_palette;
+	  QColor l_color = l_palette.color(QPalette::Window);
+	  QColor fillColor = colorBetween(l_color, active->get_color(), data.getVolumeValue(*gdata));
+	  QColor outlineColor = colorBetween(l_color, Qt::black, data.getVolumeValue(*gdata));
 	  get_painter().setBrush(fillColor);
   
 	  int barStart = 0;

widgets/tuner/tunerwidget.cpp
@@ -72,8 +72,9 @@ void TunerWidget::paintEvent(QPaintEvent *)
     lines would eventually meet up with the radius of the circle (width()/2, radius).
 
   */
-
-  QPen pen(colorGroup().foreground(), 2);
+  QPalette l_palette;
+  QColor l_color = l_palette.color(QPalette::WindowText);
+  QPen pen(l_color, 2);
   // Border
   get_painter().setPen(pen);
 
@@ -92,8 +93,8 @@ void TunerWidget::paintEvent(QPaintEvent *)
     get_painter().drawArc(toInt(halfWidth - (radius / 2.0)), toInt(radius / 2.0), toInt(radius), toInt(radius), toInt((90 - thetaDeg) * 16), toInt(2 * thetaDeg * 16));
   }
  
-  get_painter().setPen(colorGroup().foreground());
-  get_painter().setBrush(colorGroup().foreground());
+  get_painter().setPen(l_color);
+  get_painter().setBrush(l_color);
   
     
   double step = (2 * theta) / 12.0;
@@ -111,7 +112,7 @@ void TunerWidget::paintEvent(QPaintEvent *)
   
   {
     //Draw the text labels
-    get_painter().setPen(colorGroup().foreground());
+    get_painter().setPen(l_color);
   
     const char *theNames[11] = { "+50", "+40", "+30", "+20", "+10", "0", "-10", "-20", "-30", "-40", "-50" };
     QFontMetrics fm = get_painter().fontMetrics();

nsauzede/tartini_exp

widgets/freq/freqdrawwidget.cpp
@@ -25,6 +25,7 @@
 #include <QKeyEvent>
 #include <QEvent>
 #include <QPaintEvent>
+#include <QPalette>
 
 // zoom cursors
 #include "pics/zoomx.xpm"
@@ -184,10 +185,12 @@ void FreqDrawWidget::paintEvent(QPaintEvent *)
         drawChannel(*this, l_channel, get_painter(), l_view.viewLeft(), l_view.currentTime(), l_view.zoomX(), l_view.viewBottom(), l_view.zoomY(), DRAW_VIEW_NORMAL);
     }
 
+    QPalette l_palette;
+    QColor l_foreground = l_palette.color(QPalette::WindowText);
     // Draw a light grey band indicating which time is being used in the current window
     if(gdata->getActiveChannel())
     {
-        QColor l_line_color = colorGroup().foreground();
+        QColor l_line_color = l_foreground;
         l_line_color.setAlpha(50);
         Channel * l_channel = gdata->getActiveChannel();
         double l_half_window_time = (double)l_channel->size() / (double)(l_channel->rate() * 2);
@@ -197,7 +200,7 @@ void FreqDrawWidget::paintEvent(QPaintEvent *)
     }
 
     // Draw the current time line
-    get_painter().setPen(QPen(colorGroup().foreground(), 1));
+    get_painter().setPen(QPen(l_foreground, 1));
     get_painter().drawLine(l_cur_time_pixel, 0, l_cur_time_pixel, height() - 1);
 
     endDrawing();

widgets/hblock/hblockwidget.cpp
@@ -58,8 +58,10 @@ void HBlockWidget::paintEvent( QPaintEvent * )
   
             // Work out the bar height for each harmonic
             double l_bar_height = double(height()) / double(l_data.getHarmonicFreqSize());
-            QColor l_fill_color = colorBetween(colorGroup().background(), l_active_channel->get_color(), l_data.getVolumeValue(*gdata));
-            QColor l_outline_color = colorBetween(colorGroup().background(), Qt::black, l_data.getVolumeValue(*gdata));
+            QPalette l_palette;
+            QColor l_color = l_palette.color(QPalette::Window);
+            QColor l_fill_color = colorBetween(l_color, l_active_channel->get_color(), l_data.getVolumeValue(*gdata));
+            QColor l_outline_color = colorBetween(l_color, Qt::black, l_data.getVolumeValue(*gdata));
             get_painter().setBrush(l_fill_color);
   
             int l_bar_start = 0;

widgets/tuner/tunerwidget.cpp
@@ -73,7 +73,9 @@ void TunerWidget::paintEvent(QPaintEvent *)
 
      */
 
-    QPen l_pen(colorGroup().foreground(), 2);
+    QPalette l_palette;
+    QColor l_color = l_palette.color(QPalette::WindowText);
+    QPen l_pen(l_color, 2);
     // Border
     get_painter().setPen(l_pen);
 
@@ -92,9 +94,9 @@ void TunerWidget::paintEvent(QPaintEvent *)
         get_painter().drawArc(toInt(l_half_width - (l_radius / 2.0)), toInt(l_radius / 2.0), toInt(l_radius), toInt(l_radius), toInt((90 - l_theta_deg) * 16), toInt(2 * l_theta_deg * 16));
     }
 
-    get_painter().setPen(colorGroup().foreground());
-    get_painter().setBrush(colorGroup().foreground());
-
+    get_painter().setPen(l_color);
+    get_painter().setBrush(l_color);
+    
     double l_step = (2 * l_theta) / 12.0;
     double l_stop = l_rho + (2 * l_theta) - (l_step / 2);
     {
@@ -111,7 +113,7 @@ void TunerWidget::paintEvent(QPaintEvent *)
 
     {
         //Draw the text labels
-        get_painter().setPen(colorGroup().foreground());
+        get_painter().setPen(l_color);
 
         const char *l_the_names[11] = { "+50", "+40", "+30", "+20", "+10", "0", "-10", "-20", "-30", "-40", "-50" };
         QFontMetrics l_font_metrics = get_painter().fontMetrics();
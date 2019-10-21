ryanbekabe/tartini

Migrate Qt3 code related object name to Qt4 code

widgets/myscrollbar.cpp
@@ -20,9 +20,10 @@
 
 //------------------------------------------------------------------------------
 MyScrollBar::MyScrollBar(Qt::Orientation orientation, QWidget *parent, const char *name):
-  QWidget(parent, name),
+  QWidget(parent),
   bar(new QScrollBar(orientation, this))
 {
+  setObjectName(name);
   bar->show();
 }
 
@@ -35,8 +36,9 @@ MyScrollBar::MyScrollBar(double minValue_,
 			 double step_,
 			 Qt::Orientation orientation,
 			 QWidget * parent,
-			 const char * name):
-  QWidget(parent, name),
+			 const char * name
+			 ):
+  QWidget(parent),
   _minValue(minValue_),
   _maxValue(maxValue_),
   _value(value_),
@@ -45,6 +47,7 @@ MyScrollBar::MyScrollBar(double minValue_,
   _step(step_),
   bar(new QScrollBar(orientation, this))
 {
+  setObjectName(name);
   bar->setMinimum(toInt(_minValue*_step));
   bar->setMaximum(toInt(_maxValue*_step));
   bar->setSingleStep(MAX(toInt(_lineStep*_step), 1));

nsauzede/tartini_exp

widgets/myscrollbar.cpp
@@ -25,9 +25,10 @@ MyScrollBar::MyScrollBar(Qt::Orientation p_orientation
                         ,QWidget * p_parent
                         ,const char * p_name
                         )
-: QWidget(p_parent, p_name)
+: QWidget(p_parent)
 , m_bar(new QScrollBar(p_orientation, this))
 {
+    setObjectName(p_name);
     m_bar->show();
 }
 
@@ -42,7 +43,7 @@ MyScrollBar::MyScrollBar(double p_min_value
                         ,QWidget * p_parent
                         ,const char * p_name
                         )
-: QWidget(p_parent, p_name)
+: QWidget(p_parent)
 , m_min_value(p_min_value)
 , m_max_value(p_max_value)
 , m_value(p_value)
@@ -51,6 +52,7 @@ MyScrollBar::MyScrollBar(double p_min_value
   m_step(p_step),
   m_bar(new QScrollBar(p_orientation, this))
 {
+    setObjectName(p_name);
     m_bar->setMinimum(toInt(m_min_value * m_step));
     m_bar->setMaximum(toInt(m_max_value * m_step));
     m_bar->setSingleStep(MAX(toInt(m_line_step * m_step), 1));
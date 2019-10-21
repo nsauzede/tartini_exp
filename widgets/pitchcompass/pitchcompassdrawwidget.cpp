ryanbekabe/tartini

Migrate Qt3 code related to destruction on close to Qt4 code

widgets/pitchcompass/pitchcompassdrawwidget.cpp
@@ -31,8 +31,9 @@
 
 //------------------------------------------------------------------------------
 PitchCompassDrawWidget::PitchCompassDrawWidget(QWidget *parent, const char* name, int mode)
-  : QWidget(parent, name, Qt::WDestructiveClose)
+  : QWidget(parent)
 {
+  setAttribute(Qt::WA_DeleteOnClose);
     this->mode = mode;
 
     compass = new QwtCompass(this);

nsauzede/tartini_exp

widgets/pitchcompass/pitchcompassdrawwidget.cpp
@@ -36,8 +36,9 @@ PitchCompassDrawWidget::PitchCompassDrawWidget( QWidget *p_parent
                                               , const char* p_name
                                               , int p_mode
                                               )
-: QWidget(p_parent, p_name, Qt::WDestructiveClose)
+: QWidget(p_parent)
 {
+    setAttribute(Qt::WA_DeleteOnClose);
     this->m_mode = p_mode;
 
     m_compass = new QwtCompass(this);
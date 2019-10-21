ryanbekabe/tartini

Migrate Qt3 code to Qt4 code _ Remove deprecated code about MainWindow main Widget _ Remove call to deprecated QLabel constructor

sound/soundfile.cpp
@@ -603,9 +603,9 @@ void SoundFile::preProcess(void)
   readChunk(bufferSize() - offset());
 
   // Create a progress bar in the status bar to tell the user we're preprocessing
-  MainWindow *theMainWindow = (MainWindow*) qApp->mainWidget();
+  MainWindow *theMainWindow = mainWindow;
   QStatusBar *theStatusBar = theMainWindow->statusBar();
-  QLabel *message = new QLabel("Preprocessing data:", theStatusBar, "message");
+  QLabel *message = new QLabel("Preprocessing data:", theStatusBar);
 
   QProgressBar *progress = new QProgressBar(theStatusBar);
   progress->setRange(0,stream->totalFrames() / framesPerChunk() - 1);

nsauzede/tartini_exp

sound/soundfile.cpp
@@ -641,9 +641,9 @@ void SoundFile::preProcess(void)
     readChunk(bufferSize() - offset());
 
     // Create a progress bar in the status bar to tell the user we're preprocessing
-    MainWindow * l_the_main_window = (MainWindow*) qApp->mainWidget();
+    MainWindow *l_the_main_window = g_main_window;
     QStatusBar * l_the_status_bar = l_the_main_window->statusBar();
-    QLabel * l_message = new QLabel("Preprocessing data:", l_the_status_bar, "message");
+    QLabel * l_message = new QLabel("Preprocessing data:", l_the_status_bar);
 
     QProgressBar * l_progress = new QProgressBar(l_the_status_bar);
     l_progress->setRange(0,m_stream->totalFrames() / framesPerChunk() - 1);
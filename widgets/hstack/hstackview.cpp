ryanbekabe/tartini

Migrate Qt3 code to Qt4 _ Replace Q3VBoxLayout by QVBoxLayout _ Replace Q3HBoxLayout by QHBoxLayout _ Replace Q3Grid by QFrame _ Refresh include directives to Qt4 include style

widgets/hstack/hstackview.cpp
@@ -21,19 +21,18 @@
 #include "useful.h" 
 #include "myscrollbar.h"
 
-#include <qlayout.h>
-#include <qpushbutton.h>
-#include <qsizegrip.h>
-#include <qsplitter.h>
-#include <qtooltip.h>
-#include <q3grid.h>
+#include <QLayout>
+#include <QPushButton>
+#include <QSizeGrip>
+#include <QSplitter>
+#include <QToolTip>
 #include <qwt_wheel.h>
-#include <qcursor.h>
-#include <qcombobox.h>
-#include <qspinbox.h>
-#include <Q3VBoxLayout>
-#include <Q3HBoxLayout>
-#include <Q3Frame>
+#include <QCursor>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFrame>
 
 //------------------------------------------------------------------------------
 HStackView::HStackView( int viewID_, QWidget * parent):
@@ -41,17 +40,17 @@ HStackView::HStackView( int viewID_, QWidget * parent):
 {
   setCaption("Harmonic Stack");
 
-  Q3BoxLayout * mainLayout = new Q3HBoxLayout(this);
+  QHBoxLayout * mainLayout = new QHBoxLayout(this);
 
-  Q3BoxLayout * leftLayout = new Q3VBoxLayout(mainLayout);
-  Q3BoxLayout * rightLayout = new Q3VBoxLayout(mainLayout);
+  QVBoxLayout * leftLayout = new QVBoxLayout(mainLayout);
+  QVBoxLayout * rightLayout = new QVBoxLayout(mainLayout);
 
-  Q3Grid * waveFrame = new Q3Grid(1, this);
-  waveFrame->setFrameStyle(Q3Frame::WinPanel | Q3Frame::Sunken);
+  QFrame * waveFrame = new QFrame(this);
+  waveFrame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
   leftLayout->addWidget(waveFrame);
   hStackWidget = new HStackWidget(waveFrame);
   
-  Q3BoxLayout * bottomLayout = new Q3HBoxLayout(leftLayout);
+  QHBoxLayout * bottomLayout = new QHBoxLayout(leftLayout);
 
   QwtWheel * dbRangeWheel = new QwtWheel(this);
   dbRangeWheel->setOrientation(Qt::Vertical);

nsauzede/tartini_exp

widgets/hstack/hstackview.cpp
@@ -21,19 +21,18 @@
 #include "useful.h" 
 #include "myscrollbar.h"
 
-#include <qlayout.h>
-#include <qpushbutton.h>
-#include <qsizegrip.h>
-#include <qsplitter.h>
-#include <qtooltip.h>
-#include <q3grid.h>
+#include <QLayout>
+#include <QPushButton>
+#include <QSizeGrip>
+#include <QSplitter>
+#include <QToolTip>
 #include <qwt_wheel.h>
-#include <qcursor.h>
-#include <qcombobox.h>
-#include <qspinbox.h>
-#include <Q3VBoxLayout>
-#include <Q3HBoxLayout>
-#include <Q3Frame>
+#include <QCursor>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFrame>
 
 //------------------------------------------------------------------------------
 HStackView::HStackView( int p_view_id
@@ -43,17 +42,17 @@ HStackView::HStackView( int p_view_id
 {
     setCaption("Harmonic Stack");
 
-    Q3BoxLayout * l_main_layout = new Q3HBoxLayout(this);
+    QHBoxLayout * l_main_layout = new QHBoxLayout(this);
 
-    Q3BoxLayout * l_left_layout = new Q3VBoxLayout(l_main_layout);
-    Q3BoxLayout * l_right_layout = new Q3VBoxLayout(l_main_layout);
+    QVBoxLayout * l_left_layout = new QVBoxLayout(l_main_layout);
+    QVBoxLayout * l_right_layout = new QVBoxLayout(l_main_layout);
 
-    Q3Grid * l_wave_frame = new Q3Grid(1, this);
-    l_wave_frame->setFrameStyle(Q3Frame::WinPanel | Q3Frame::Sunken);
+    QFrame * l_wave_frame = new QFrame(this);
+    l_wave_frame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
     l_left_layout->addWidget(l_wave_frame);
     m_h_stack_widget = new HStackWidget(l_wave_frame);
 
-    Q3BoxLayout * l_bottom_layout = new Q3HBoxLayout(l_left_layout);
+    QHBoxLayout * l_bottom_layout = new QHBoxLayout(l_left_layout);
 
     QwtWheel * l_db_range_wheel = new QwtWheel(this);
     l_db_range_wheel->setOrientation(Qt::Vertical);
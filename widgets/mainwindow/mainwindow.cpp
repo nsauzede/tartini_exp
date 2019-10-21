ryanbekabe/tartini

Migrate Qt3 code related object name to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -1158,7 +1158,8 @@ TartiniDialog::TartiniDialog(QWidget *parent):
   QVBoxLayout * layout = new QVBoxLayout(this, 6, 6);
 
   QHBoxLayout * topLayout = new QHBoxLayout(layout);
-  QLabel * tartiniPicture = new QLabel(this, "TartiniPicture");
+  QLabel * tartiniPicture = new QLabel(this);
+  tartiniPicture->setObjectName("TartiniPicture");
   QPixmap tartiniPixmap(":/res/tartinilogo.jpg", "JPG");
   tartiniPicture->setPixmap(tartiniPixmap);
   tartiniPicture->setBackgroundColor(Qt::black);
@@ -1256,7 +1257,8 @@ GPLDialog::GPLDialog(QWidget *parent):
   setCaption("GPL Licence");
   QVBoxLayout * layout = new QVBoxLayout(this, 6, 6);
 
-  QTextEdit * GPLTextEdit = new QTextEdit(this, "GPLTextEdit");
+  QTextEdit * GPLTextEdit = new QTextEdit(this);
+  GPLTextEdit->setObjectName("GPLTextEdit");
   GPLTextEdit->setReadOnly(true);
   GPLTextEdit->setAcceptRichText(false);
   QString theText;

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -1200,7 +1200,8 @@ TartiniDialog::TartiniDialog(QWidget *p_parent)
     QVBoxLayout * l_layout = new QVBoxLayout(this, 6, 6);
 
     QHBoxLayout * l_top_layout = new QHBoxLayout(l_layout);
-    QLabel * l_tartini_picture = new QLabel(this, "TartiniPicture");
+    QLabel * l_tartini_picture = new QLabel(this);
+    l_tartini_picture->setObjectName("TartiniPicture");
     QPixmap l_tartini_pixmap(":/res/tartinilogo.jpg", "JPG");
     l_tartini_picture->setPixmap(l_tartini_pixmap);
     l_tartini_picture->setBackgroundColor(Qt::black);
@@ -1298,7 +1299,8 @@ GPLDialog::GPLDialog(QWidget *p_parent)
     setCaption("GPL Licence");
     QVBoxLayout * l_layout = new QVBoxLayout(this, 6, 6);
 
-    QTextEdit * l_GPL_text_edit = new QTextEdit(this, "GPLTextEdit");
+    QTextEdit * l_GPL_text_edit = new QTextEdit(this);
+    l_GPL_text_edit->setObjectName("GPLTextEdit");
     l_GPL_text_edit->setReadOnly(true);
     l_GPL_text_edit->setAcceptRichText(false);
     QString l_the_text;
ryanbekabe/tartini

Migrate Qt3 code related to QHBoxLayout to Qt4 code

widgets/mainwindow/mainwindow.cpp
@@ -1153,18 +1153,20 @@ void MainWindow::showDocumentation(void)
 TartiniDialog::TartiniDialog(QWidget *parent):
   QDialog(parent)
 {
-  QHBoxLayout * topLayout = new QHBoxLayout(layout);
   setModal(true);
   setWindowTitle("About Tartini - Version " TARTINI_VERSION_STR);
   setBackgroundRole(QPalette::Shadow);
   QVBoxLayout * layout = new QVBoxLayout(this);
   layout->setContentsMargins(6,6,6,6);
   layout->setSpacing(6);
+
+  QHBoxLayout * topLayout = new QHBoxLayout();
+  layout->addLayout(topLayout);
   QLabel * tartiniPicture = new QLabel(this);
   tartiniPicture->setObjectName("TartiniPicture");
   QPixmap tartiniPixmap(":/res/tartinilogo.jpg", "JPG");
   tartiniPicture->setPixmap(tartiniPixmap);
-  tartiniPicture->setBackgroundColor(Qt::black);
+  tartiniPicture->setBackgroundRole(QPalette::Shadow);
   topLayout->addStretch(10);
   topLayout->addWidget(tartiniPicture);
   topLayout->addStretch(10);

nsauzede/tartini_exp

widgets/mainwindow/mainwindow.cpp
@@ -1201,13 +1201,15 @@ TartiniDialog::TartiniDialog(QWidget *p_parent)
     QVBoxLayout * l_layout = new QVBoxLayout(this);
     l_layout->setContentsMargins(6,6,6,6);
     l_layout->setSpacing(6);
-    QHBoxLayout * l_top_layout = new QHBoxLayout(l_layout);
     setBackgroundRole(QPalette::Shadow);
+
+    QHBoxLayout * l_top_layout = new QHBoxLayout();
+    l_layout->addLayout(l_top_layout);
     QLabel * l_tartini_picture = new QLabel(this);
     l_tartini_picture->setObjectName("TartiniPicture");
     QPixmap l_tartini_pixmap(":/res/tartinilogo.jpg", "JPG");
     l_tartini_picture->setPixmap(l_tartini_pixmap);
-    l_tartini_picture->setBackgroundColor(Qt::black);
+    l_tartini_picture->setBackgroundRole(QPalette::Shadow);
     l_top_layout->addStretch(10);
     l_top_layout->addWidget(l_tartini_picture);
     l_top_layout->addStretch(10);
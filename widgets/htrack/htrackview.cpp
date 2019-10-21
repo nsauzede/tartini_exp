ryanbekabe/tartini

Migrate Qt3 code related to QGridLayout to Qt4

widgets/htrack/htrackview.cpp
@@ -34,7 +34,7 @@
 HTrackView::HTrackView( int viewID_, QWidget *parent ):
   ViewWidget( viewID_, parent)
 {
-  QGridLayout * mainLayout = new QGridLayout(this, 2, 2);
+  QGridLayout * mainLayout = new QGridLayout(this);
   mainLayout->setResizeMode(QLayout::SetNoConstraint);
   QVBoxLayout * rightLayout = new QVBoxLayout();
   QHBoxLayout * bottomLayout = new QHBoxLayout();

widgets/tuner/tunerview.cpp
@@ -51,12 +51,13 @@ TunerView::TunerView(int viewID_, QWidget *parent ):
   ViewWidget( viewID_, parent)
 {
 
-  QGridLayout * layout = new QGridLayout(this, 9, 3, 2);
-  layout->setResizeMode(QLayout::SetNoConstraint);
+  QGridLayout * layout = new QGridLayout(this);
+  layout->setSpacing(2);
+  layout->setSizeConstraint(QLayout::SetNoConstraint);
 
   // Tuner widget goes from (0, 0) to (0, 8);
   tunerWidget = new VibratoTunerWidget(this);
-  layout->addMultiCellWidget(tunerWidget, 0, 0, 0, 8);
+  layout->addWidget(tunerWidget, 0, 0, 1, 8);
 
   // Slider goes from (2,0) to (2,9)
 
@@ -67,7 +68,7 @@ TunerView::TunerView(int viewID_, QWidget *parent ):
 #endif
   slider->setRange(0, 2);
   slider->setReadOnly(false);
-  layout->addMultiCellWidget(slider, 1, 1, 0, 8);
+  layout->addWidget(slider, 1, 0, 1, 8);
   slider->setToolTip(tr("Increase slider to smooth the pitch over a longer time period"));
 
   ledBuffer = new QPixmap();

nsauzede/tartini_exp

widgets/htrack/htrackview.cpp
@@ -36,7 +36,7 @@ HTrackView::HTrackView( int p_view_ID
                       )
 : ViewWidget( p_view_ID, p_parent)
 {
-    QGridLayout * l_main_layout = new QGridLayout(this, 2, 2);
+    QGridLayout * l_main_layout = new QGridLayout(this);
     l_main_layout->setResizeMode(QLayout::SetNoConstraint);
     QVBoxLayout * l_right_layout = new QVBoxLayout();
     QHBoxLayout * l_bottom_layout = new QHBoxLayout();

widgets/tuner/tunerview.cpp
@@ -55,12 +55,13 @@ TunerView::TunerView(int p_view_iD_
             )
 {
 
-    QGridLayout * l_layout = new QGridLayout(this, 9, 3, 2);
-    l_layout->setResizeMode(QLayout::SetNoConstraint);
+    QGridLayout * l_layout = new QGridLayout(this);
+    l_layout->setSpacing(2);
+    l_layout->setSizeConstraint(QLayout::SetNoConstraint);
 
     // Tuner widget goes from (0, 0) to (0, 8);
     m_tuner_widget = new VibratoTunerWidget(this);
-    l_layout->addMultiCellWidget(m_tuner_widget, 0, 0, 0, 8);
+    l_layout->addWidget(m_tuner_widget, 0, 0, 1, 8);
 
     // Slider goes from (2,0) to (2,9)
 
@@ -79,7 +80,7 @@ TunerView::TunerView(int p_view_iD_
     m_slider->setRange(0, 2);
 #endif // QWT_VERSION >= 0x060000
     m_slider->setReadOnly(false);
-    l_layout->addMultiCellWidget(m_slider, 1, 1, 0, 8);
+    l_layout->addWidget(m_slider, 1, 0, 1, 8);
     m_slider->setToolTip(tr("Increase slider to smooth the pitch over a longer time period"));
 
     m_led_buffer = new QPixmap();
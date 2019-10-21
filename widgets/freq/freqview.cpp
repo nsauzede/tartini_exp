ryanbekabe/tartini

Migrate Qt3 code related to QLayout resizing mode to Qt4 code

widgets/freq/freqview.cpp
@@ -47,7 +47,7 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   View & view = gdata->getView();
 
   QVBoxLayout *mainLayout = new QVBoxLayout(this);
-  mainLayout->setResizeMode(QLayout::SetNoConstraint);
+  mainLayout->setSizeConstraint(QLayout::SetNoConstraint);
 
 
   QSplitter * splitter = new QSplitter(Qt::Vertical, this);

widgets/htrack/htrackview.cpp
@@ -35,7 +35,7 @@ HTrackView::HTrackView( int viewID_, QWidget *parent ):
   ViewWidget( viewID_, parent)
 {
   QGridLayout * mainLayout = new QGridLayout(this);
-  mainLayout->setResizeMode(QLayout::SetNoConstraint);
+  mainLayout->setSizeConstraint(QLayout::SetNoConstraint);
   QVBoxLayout * rightLayout = new QVBoxLayout();
   QHBoxLayout * bottomLayout = new QHBoxLayout();
 

nsauzede/tartini_exp

widgets/freq/freqview.cpp
@@ -51,7 +51,7 @@ FreqView::FreqView( int p_view_id
     View & l_view = gdata->getView();
 
     QVBoxLayout *l_main_layout = new QVBoxLayout(this);
-    l_main_layout->setResizeMode(QLayout::SetNoConstraint);
+    l_main_layout->setSizeConstraint(QLayout::SetNoConstraint);
 
     QSplitter * l_splitter = new QSplitter(Qt::Vertical, this);
     QWidget * l_top_widget = new QWidget(l_splitter);

widgets/htrack/htrackview.cpp
@@ -37,7 +37,7 @@ HTrackView::HTrackView( int p_view_ID
 : ViewWidget( p_view_ID, p_parent)
 {
     QGridLayout * l_main_layout = new QGridLayout(this);
-    l_main_layout->setResizeMode(QLayout::SetNoConstraint);
+    l_main_layout->setSizeConstraint(QLayout::SetNoConstraint);
     QVBoxLayout * l_right_layout = new QVBoxLayout();
     QHBoxLayout * l_bottom_layout = new QHBoxLayout();
 
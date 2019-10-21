ryanbekabe/tartini

Migrate Qt3 code managing intricated layouts to Qt4

widgets/freq/freqview.cpp
@@ -53,7 +53,8 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   QSplitter * splitter = new QSplitter(Qt::Vertical, this);
   QWidget * topWidget = new QWidget(splitter);
   QHBoxLayout * topLayout = new QHBoxLayout(topWidget);
-  QVBoxLayout * topLeftLayout = new QVBoxLayout(topLayout);
+  QVBoxLayout * topLeftLayout = new QVBoxLayout();
+  topLayout->addLayout(topLeftLayout);
   
   timeAxis = new TimeAxis(topWidget, gdata->leftTime(), gdata->rightTime(), true);
   timeAxis->setWhatsThis("The time in seconds");
@@ -97,8 +98,9 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   
   QWidget * bottomWidget = new QWidget(splitter);
   QVBoxLayout * bottomLayout = new QVBoxLayout(bottomWidget);
-  QHBoxLayout * bottomTopLayout = new QHBoxLayout(bottomLayout);
-  
+  QHBoxLayout * bottomTopLayout = new QHBoxLayout();
+  bottomLayout->addLayout(bottomTopLayout);
+
   QFrame * amplitudeFrame = new QFrame;
   amplitudeFrame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
   QVBoxLayout * amplitudeFrameLayout = new QVBoxLayout;
@@ -110,8 +112,9 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   amplitudeFrame->setLayout(amplitudeFrameLayout);
   bottomTopLayout->addWidget(amplitudeFrame);
 
-  QVBoxLayout *bottomTopRightLayout = new QVBoxLayout(bottomTopLayout);
-  
+  QVBoxLayout *bottomTopRightLayout = new QVBoxLayout();
+  bottomTopLayout->addLayout(bottomTopRightLayout);
+
   amplitudeWheelY = new QwtWheel(bottomWidget);
   amplitudeWheelY->setOrientation(Qt::Vertical);
   amplitudeWheelY->setWheelWidth(14);
@@ -124,7 +127,8 @@ FreqView::FreqView(int viewID_, QWidget *parent):
   amplitudeScrollBar = new MyScrollBar(0.0, 1.0 - amplitudeWidget->range(), 0.20, amplitudeWidget->range(), 0, 20, Qt::Vertical, bottomWidget);
   bottomTopRightLayout->addWidget(amplitudeScrollBar, 4);
   
-  QHBoxLayout * bottomBottomLayout = new QHBoxLayout(bottomLayout);
+  QHBoxLayout * bottomBottomLayout = new QHBoxLayout();
+  bottomLayout->addLayout(bottomBottomLayout);
   
   QComboBox * amplitudeModeComboBox = new QComboBox(bottomWidget, "amplitudeTypeModeBox");
   amplitudeModeComboBox->setWhatsThis("Select different algorithm parameters to view in the bottom pannel");

widgets/hcircle/hcircleview.cpp
@@ -42,8 +42,10 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
 
   QHBoxLayout * mainLayout = new QHBoxLayout(this);
 
-  QVBoxLayout * leftLayout = new QVBoxLayout(mainLayout);
-  QVBoxLayout * rightLayout = new QVBoxLayout(mainLayout);
+  QVBoxLayout * leftLayout = new QVBoxLayout();
+  mainLayout->addLayout(leftLayout);
+  QVBoxLayout * rightLayout = new QVBoxLayout();
+  mainLayout->addLayout(rightLayout);
  
   QFrame * waveFrame = new QFrame(this);
   waveFrame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
@@ -51,7 +53,8 @@ HCircleView::HCircleView( int viewID_, QWidget *parent):
 
   hCircleWidget = new HCircleWidget(waveFrame);
 
-  QHBoxLayout * bottomLayout = new QHBoxLayout(leftLayout);
+  QHBoxLayout * bottomLayout = new QHBoxLayout();
+  leftLayout->addLayout(bottomLayout);
  
   QwtWheel * ZoomWheel = new QwtWheel(this);
   ZoomWheel->setOrientation(Qt::Vertical);

widgets/hstack/hstackview.cpp
@@ -42,15 +42,18 @@ HStackView::HStackView( int viewID_, QWidget * parent):
 
   QHBoxLayout * mainLayout = new QHBoxLayout(this);
 
-  QVBoxLayout * leftLayout = new QVBoxLayout(mainLayout);
-  QVBoxLayout * rightLayout = new QVBoxLayout(mainLayout);
+  QVBoxLayout * leftLayout = new QVBoxLayout();
+  mainLayout->addLayout(leftLayout);
+  QVBoxLayout * rightLayout = new QVBoxLayout();
+  mainLayout->addLayout(rightLayout);
 
   QFrame * waveFrame = new QFrame(this);
   waveFrame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
   leftLayout->addWidget(waveFrame);
   hStackWidget = new HStackWidget(waveFrame);
   
-  QHBoxLayout * bottomLayout = new QHBoxLayout(leftLayout);
+  QHBoxLayout * bottomLayout = new QHBoxLayout();
+  leftLayout->addLayout(bottomLayout);
 
   QwtWheel * dbRangeWheel = new QwtWheel(this);
   dbRangeWheel->setOrientation(Qt::Vertical);

widgets/mainwindow/mainwindow.cpp
@@ -1217,7 +1217,8 @@ TartiniDialog::TartiniDialog(QWidget *parent):
   
   layout->addWidget(tartiniTextEdit);
   
-  QHBoxLayout * bottomLayout = new QHBoxLayout(layout);
+  QHBoxLayout * bottomLayout = new QHBoxLayout();
+  layout->addLayout(bottomLayout);
   QPushButton * okButton = new QPushButton("&Ok", this);
   QPushButton * GPLButton = new QPushButton("&Read License", this);
   bottomLayout->addStretch(10);
@@ -1252,7 +1253,8 @@ GPLDialog::GPLDialog(QWidget *parent):
   
   layout->addWidget(GPLTextEdit);
   
-  QHBoxLayout * bottomLayout = new QHBoxLayout(layout);
+  QHBoxLayout * bottomLayout = new QHBoxLayout();
+  layout->addLayout(bottomLayout);
   QPushButton * okButton = new QPushButton("&Ok", this);
   bottomLayout->addStretch(10);
   bottomLayout->addWidget(okButton);

widgets/score/scoreview.cpp
@@ -33,13 +33,16 @@ ScoreView::ScoreView(int viewID_, QWidget * parent):
 
   QHBoxLayout * mainLayout = new QHBoxLayout(this);
 
-  QVBoxLayout * leftLayout = new QVBoxLayout(mainLayout);
-  QVBoxLayout * rightLayout = new QVBoxLayout(mainLayout);
+  QVBoxLayout * leftLayout = new QVBoxLayout();
+  mainLayout->addLayout(leftLayout);
+  QVBoxLayout * rightLayout = new QVBoxLayout();
+  mainLayout->addLayout(rightLayout);
 
   //create a drawing object
   scoreWidget = new ScoreWidget(this);
   leftLayout->addWidget(scoreWidget);
-  QHBoxLayout * bottomLayout = new QHBoxLayout(leftLayout);
+  QHBoxLayout * bottomLayout = new QHBoxLayout();
+  leftLayout->addLayout(bottomLayout);
 
   //vertical scroll wheel
   QwtWheel *scaleWheelY = new QwtWheel(this);

nsauzede/tartini_exp

widgets/freq/freqview.cpp
@@ -56,7 +56,8 @@ FreqView::FreqView( int p_view_id
     QSplitter * l_splitter = new QSplitter(Qt::Vertical, this);
     QWidget * l_top_widget = new QWidget(l_splitter);
     QHBoxLayout * l_top_layout = new QHBoxLayout(l_top_widget);
-    QVBoxLayout * l_top_left_layout = new QVBoxLayout(l_top_layout);
+    QVBoxLayout * l_top_left_layout = new QVBoxLayout();
+    l_top_layout->addLayout(l_top_left_layout);
   
     m_time_axis = new TimeAxis(l_top_widget, gdata->leftTime(), gdata->rightTime(), true);
     m_time_axis->setWhatsThis("The time in seconds");
@@ -105,7 +106,8 @@ FreqView::FreqView( int p_view_id
     //------------bottom half------------------
     QWidget * l_bottom_widget = new QWidget(l_splitter);
     QVBoxLayout * l_bottom_layout = new QVBoxLayout(l_bottom_widget);
-    QHBoxLayout * l_bottom_top_layout = new QHBoxLayout(l_bottom_layout);
+    QHBoxLayout * l_bottom_top_layout = new QHBoxLayout();
+    l_bottom_layout->addLayout(l_bottom_top_layout);
 
     QFrame * l_amplitude_frame = new QFrame;
     l_amplitude_frame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
@@ -118,7 +120,8 @@ FreqView::FreqView( int p_view_id
     l_amplitude_frame->setLayout(l_amplitude_frame_layout);
     l_bottom_top_layout->addWidget(l_amplitude_frame);
 
-    QVBoxLayout *l_bottom_top_right_layout = new QVBoxLayout(l_bottom_top_layout);
+    QVBoxLayout *l_bottom_top_right_layout = new QVBoxLayout();
+    l_bottom_top_layout->addLayout(l_bottom_top_right_layout);
 
     m_amplitude_wheel_Y = new QwtWheel(l_bottom_widget);
     m_amplitude_wheel_Y->setOrientation(Qt::Vertical);
@@ -139,7 +142,8 @@ FreqView::FreqView( int p_view_id
     m_amplitude_scroll_bar = new MyScrollBar(0.0, 1.0 - m_amplitude_widget->range(), 0.20, m_amplitude_widget->range(), 0, 20, Qt::Vertical, l_bottom_widget);
     l_bottom_top_right_layout->addWidget(m_amplitude_scroll_bar, 4);
 
-    QHBoxLayout * l_bottom_bottom_layout = new QHBoxLayout(l_bottom_layout);
+    QHBoxLayout * l_bottom_bottom_layout = new QHBoxLayout();
+    l_bottom_layout->addLayout(l_bottom_bottom_layout);
 
     QComboBox * l_amplitude_mode_combo_box = new QComboBox(l_bottom_widget, "amplitudeTypeModeBox");
     l_amplitude_mode_combo_box->setWhatsThis("Select different algorithm parameters to view in the bottom pannel");

widgets/hcircle/hcircleview.cpp
@@ -44,8 +44,10 @@ HCircleView::HCircleView( int p_view_id
 
     QHBoxLayout * l_main_layout = new QHBoxLayout(this);
 
-    QVBoxLayout * l_left_layout = new QVBoxLayout(l_main_layout);
-    QVBoxLayout * l_right_layout = new QVBoxLayout(l_main_layout);
+    QVBoxLayout * l_left_layout = new QVBoxLayout();
+    l_main_layout->addLayout(l_left_layout);
+    QVBoxLayout * l_right_layout = new QVBoxLayout();
+    l_main_layout->addLayout(l_right_layout);
 
     QFrame * l_wave_frame = new QFrame(this);
     l_wave_frame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
@@ -53,7 +55,8 @@ HCircleView::HCircleView( int p_view_id
 
     m_h_circle_widget = new HCircleWidget(l_wave_frame);
 
-    QHBoxLayout * l_bottom_layout = new QHBoxLayout(l_left_layout);
+    QHBoxLayout * l_bottom_layout = new QHBoxLayout();
+    l_left_layout->addLayout(l_bottom_layout);
  
     QwtWheel * l_zoom_wheel = new QwtWheel(this);
     l_zoom_wheel->setOrientation(Qt::Vertical);
@@ -87,7 +90,7 @@ HCircleView::HCircleView( int p_view_id
     l_lowest_value_wheel->setToolTip("Change the lowest value");
     l_right_layout->addWidget(l_lowest_value_wheel);
     l_right_layout->addStretch(2);
- 
+
     QwtWheel* l_threshold_wheel = new QwtWheel(this);
     l_threshold_wheel->setOrientation(Qt::Horizontal);
     l_threshold_wheel->setWheelWidth(14);

widgets/hstack/hstackview.cpp
@@ -44,15 +44,18 @@ HStackView::HStackView( int p_view_id
 
     QHBoxLayout * l_main_layout = new QHBoxLayout(this);
 
-    QVBoxLayout * l_left_layout = new QVBoxLayout(l_main_layout);
-    QVBoxLayout * l_right_layout = new QVBoxLayout(l_main_layout);
+    QVBoxLayout * l_left_layout = new QVBoxLayout();
+    l_main_layout->addLayout(l_left_layout);
+    QVBoxLayout * l_right_layout = new QVBoxLayout();
+    l_main_layout->addLayout(l_right_layout);
 
     QFrame * l_wave_frame = new QFrame(this);
     l_wave_frame->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
     l_left_layout->addWidget(l_wave_frame);
     m_h_stack_widget = new HStackWidget(l_wave_frame);
 
-    QHBoxLayout * l_bottom_layout = new QHBoxLayout(l_left_layout);
+    QHBoxLayout * l_bottom_layout = new QHBoxLayout();
+    l_left_layout->addLayout(l_bottom_layout);
 
     QwtWheel * l_db_range_wheel = new QwtWheel(this);
     l_db_range_wheel->setOrientation(Qt::Vertical);

widgets/mainwindow/mainwindow.cpp
@@ -1258,7 +1258,8 @@ TartiniDialog::TartiniDialog(QWidget *p_parent)
     l_tartini_text_edit->setPalette(l_palette);
     l_layout->addWidget(l_tartini_text_edit);
   
-    QHBoxLayout * l_bottom_layout = new QHBoxLayout(l_layout);
+    QHBoxLayout * l_bottom_layout = new QHBoxLayout();
+    l_layout->addLayout(l_bottom_layout);
     QPushButton * l_ok_button = new QPushButton("&Ok", this);
     QPushButton * l_GPL_button = new QPushButton("&Read License", this);
     l_bottom_layout->addStretch(10);
@@ -1293,7 +1294,8 @@ GPLDialog::GPLDialog(QWidget *p_parent)
   
     l_layout->addWidget(l_GPL_text_edit);
 
-    QHBoxLayout * l_bottom_layout = new QHBoxLayout(l_layout);
+    QHBoxLayout * l_bottom_layout = new QHBoxLayout();
+    l_layout->addLayout(l_bottom_layout);
     QPushButton * l_ok_button = new QPushButton("&Ok", this);
     l_bottom_layout->addStretch(10);
     l_bottom_layout->addWidget(l_ok_button);

widgets/score/scoreview.cpp
@@ -35,13 +35,16 @@ ScoreView::ScoreView(int p_view_id
 
     QHBoxLayout * l_main_layout = new QHBoxLayout(this);
 
-    QVBoxLayout * l_left_layout = new QVBoxLayout(l_main_layout);
-    QVBoxLayout * l_right_layout = new QVBoxLayout(l_main_layout);
+    QVBoxLayout * l_left_layout = new QVBoxLayout();
+    l_main_layout->addLayout(l_left_layout);
+    QVBoxLayout * l_right_layout = new QVBoxLayout();
+    l_main_layout->addLayout(l_right_layout);
 
     //create a drawing object
     m_score_widget = new ScoreWidget(this);
     l_left_layout->addWidget(m_score_widget);
-    QHBoxLayout * l_bottom_layout = new QHBoxLayout(l_left_layout);
+    QHBoxLayout * l_bottom_layout = new QHBoxLayout();
+    l_left_layout->addLayout(l_bottom_layout);
 
     //vertical scroll wheel
     QwtWheel *l_scale_wheel_Y = new QwtWheel(this);
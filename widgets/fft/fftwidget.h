ryanbekabe/tartini

Migrate Qt3 code to Qt4: replace Q3PointArray by QPolygon

widgets/fft/fftwidget.h
@@ -18,17 +18,14 @@
 #define FFTWIDGET_H
 
 #include "drawwidget.h"
-#include <Q3PointArray>
+#include <QPolygon>
 #include <QPaintEvent>
-
-// Forward declarations of classes the h file doesn't need to know specifics about
-class Q3PointArray;
 
 class FFTWidget: public DrawWidget
 {
   Q_OBJECT
 
-public:
+ public:
   FFTWidget(QWidget *parent);
   virtual ~FFTWidget(void);
 
@@ -37,7 +34,7 @@ public:
   QSize sizeHint(void) const;
 
  private:
-  Q3PointArray pointArray;
+  QPolygon pointArray;
 };
 
 #endif // FFTWIDGET_H

nsauzede/tartini_exp

widgets/fft/fftwidget.h
@@ -18,11 +18,8 @@
 #define FFTWIDGET_H
 
 #include "drawwidget.h"
-#include <Q3PointArray>
+#include <QPolygon>
 #include <QPaintEvent>
-
-// Forward declarations of classes the h file doesn't need to know specifics about
-class Q3PointArray;
 
 class FFTWidget: public DrawWidget
 {
@@ -37,7 +34,7 @@ class FFTWidget: public DrawWidget
     QSize sizeHint(void) const;
 
   private:
-    Q3PointArray m_point_array;
+    QPolygon m_point_array;
 };
 
 #endif // FFTWIDGET_H